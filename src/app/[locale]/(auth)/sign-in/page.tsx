3a602eedd67d46238b40a123532de58ed128f3c9
Cristo RPS <cristobits0101@gmail.com>
2024-09-24 19:35:29 +0100

    Updating imports

--- src/app/[locale]/(auth)/sign-in/page.tsx
+++ src/app/[locale]/(auth)/sign-in/page.tsx
@@ -1,7 +1,6 @@
-import LoginButton from '@/components/auth/login-button'
 import LoginForm from '@/components/auth/login-form'
 import Logo from '@/components/branding/logo'
-import styles from '@/styles/auth/sign-in.module.css'
+// import styles from '@/styles/auth/sign-in.module.css'
 
 export default function SignInPage() {
   return (
