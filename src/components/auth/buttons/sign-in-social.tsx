594dec5bfc8ad4f5630115b2ccb81a6a4735c058
Cristo RPS <cristobits0101@gmail.com>
2024-10-01 22:58:35 +0100

    Updating styles and colors inputs and social button in sign in form

--- src/components/auth/buttons/sign-in-social.tsx
+++ src/components/auth/buttons/sign-in-social.tsx
@@ -6,10 +6,10 @@ import { Button } from '@/components/ui/button'
 
 export default function SignInSocial() {
   return (
-    <div className='flex items-center w-full gap-x-10'>
+    <div className='flex items-center w-full gap-x-5'>
       <Button
         size='lg'
-        className='w-full rounded-none border-[0.05rem] border-solid border-[#bfbdc050] hover:bg-[#bfbdc050] focus:bg-[#bfbdc050] text-[#bfbdc050] focus:border-[#1d0f0f]'
+        className='w-full rounded-none border-[0.05rem] border-solid border-[#bfbdc050] hover:bg-[#bfbdc050] focus:bg-[#bfbdc050] text-[#1d0f0f] focus:border-[#1d0f0f] outline-0'
         variant='outline'
         onClick={() => {}}
       >
@@ -17,7 +17,7 @@ export default function SignInSocial() {
       </Button>
       <Button
         size='lg'
-        className='w-full rounded-none border-[0.05rem] border-solid border-[#bfbdc050] hover:bg-[#bfbdc050] focus:bg-[#bfbdc050] text-[#bfbdc050] focus:border-[#1d0f0f]'
+        className='w-full rounded-none border-[0.05rem] border-solid border-[#bfbdc050] hover:bg-[#bfbdc050] focus:bg-[#bfbdc050] text-[#1d0f0f] focus:border-[#1d0f0f] outline-0'
         variant='outline'
         onClick={() => {}}
       >
--- src/components/auth/buttons/sign-up-button.tsx
+++ src/components/auth/buttons/sign-up-button.tsx
@@ -12,7 +12,7 @@ export default function SignUpButton({ href, label }: SignUpButtonProps) {
   return (
     <Button
       variant='link'
-      className='mx-auto font-normal w-full hover:no-underline'
+      className='mx-auto font-normal w-full hover:no-underline text-[#1d0f0f] '
       asChild
     >
       <Link className='text-base' href={href}>
