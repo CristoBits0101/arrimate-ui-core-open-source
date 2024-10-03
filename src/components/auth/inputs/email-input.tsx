805614e019e3f151f37dcc7ffc57637343288a9b
Cristo RPS <cristobits0101@gmail.com>
2024-10-03 12:39:46 +0100

    Refactoring placeholder email and adding action login

--- src/components/auth/inputs/email-input.tsx
+++ src/components/auth/inputs/email-input.tsx
@@ -24,7 +24,7 @@ const EmailInput = ({ name }: EmailInputProps) => {
           <FormControl>
             <Input
               {...field}
-              placeholder='sophie@example.com'
+              placeholder='Email'
               type='email'
               className='bg-[#F4F4F4] rounded-none border-[0.05rem] border-solid border-[#bfbdc050] hover:bg-[#bfbdc050] focus:bg-[#bfbdc050] text-[#1d0f0f] placeholder:text-[#1d0f0f]'
             />
