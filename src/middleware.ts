ef74176219569274407d125854e11ef4b02c6354
Cristo RPS <cristobits0101@gmail.com>
2024-10-05 17:05:15 +0100

    Comming back

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,8 @@
-// next/server
-import { NextResponse, type NextRequest } from 'next/server'
+import createMiddleware from 'next-intl/middleware'
+import { routing } from './i18n/routing'
 
-// next-intl
-import createIntlMiddleware from 'next-intl/middleware'
-import { routing } from '@/i18n/routing'
+export default createMiddleware(routing)
 
-// next-auth
-import { auth } from '@/lib/auth'
-
-// next-intl
-const intlMiddleware = createIntlMiddleware(routing)
-
-export default async function middleware(request: NextRequest) {
-  // next-auth
-  auth((request) => {
-    const isLoggedIn = request.auth
-    console.log(`Route: ${request.nextUrl.pathname}`)
-    console.log(`isLoggedIn: ${isLoggedIn}}`)
-    NextResponse.next()
-  })
-
-  // next-intl
-  const intlResponse = intlMiddleware(request)
-  if (createIntlMiddleware(routing)) return intlResponse
-
-  // Exit
-  return NextResponse.next()
-}
-
-// Paths
 export const config = {
-  matcher: [
-    '/',
-    '/(en|es)/:path*'
-  ]
+  matcher: ['/', '/(de|en)/:path*']
 }
