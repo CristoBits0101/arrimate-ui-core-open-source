249379978366723aa59fe7b1d898ea4363c1a277
Cristo RPS <cristobits0101@gmail.com>
2024-10-05 12:10:00 +0100

    Updating middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,43 @@
-import createMiddleware from 'next-intl/middleware'
+// next/server
+import {
+  NextResponse,
+  type NextRequest,
+  type NextFetchEvent
+} from 'next/server'
+
+// next-intl
+import createIntlMiddleware from 'next-intl/middleware'
 import { routing } from '@/i18n/routing'
 
-export default createMiddleware(routing)
+// next-auth
+import { auth } from '@/lib/auth'
 
+// next-intl
+const intlMiddleware = createIntlMiddleware(routing)
+
+// next-auth
+const authMiddleware = auth(async (req: NextRequest) => {})
+
+export default async function middleware(req: NextRequest, ev: NextFetchEvent) {
+
+  // next-intl
+  const intlResponse = intlMiddleware(req)
+  if (intlResponse) return intlResponse
+
+  // next-auth
+  const authResponse = await authMiddleware(req, ev)
+  if (authResponse) return authResponse
+
+  // Exit
+  return NextResponse.next()
+
+}
+
+// Paths
 export const config = {
-  matcher: ['/', '/(en|es)/:path*']
+  matcher: [
+    '/',
+    '/(en|es)/:path*',
+    '/((?!api|_next/static|_next/image|favicon.ico).*)'
+  ]
 }
