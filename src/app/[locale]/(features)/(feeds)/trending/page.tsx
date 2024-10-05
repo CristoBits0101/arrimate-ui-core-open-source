9267b6abf4314b127bef2c1c3216e0aebbbc8e43
Cristo RPS <cristobits0101@gmail.com>
2024-10-05 14:51:03 +0100

    Deleting treding and refactoring discover to explore

--- src/app/[locale]/(features)/(feeds)/trending/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function TrendingPage() {
-  return <h2>Trending</h2>
-}
--- src/layouts/sidebar.tsx
+++ src/layouts/sidebar.tsx
@@ -1,18 +1,16 @@
 'use client'
 
-import Discover from '@/components/feeds/discover'
+import Explore from '@/components/feeds/explore'
 import Live from '@/components/streaming/live'
 import Shorts from '@/components/streaming/shorts'
 import styles from '@/styles/layouts/sidebar.module.css'
-import Trending from '@/components/feeds/trending'
 
 export default function Sidebar() {
   return (
     <aside className={styles.sidebar}>
       <nav>
         <ul>
-          <Trending />
-          <Discover />
+          <Explore />
           <Shorts />
           <Live />
         </ul>
