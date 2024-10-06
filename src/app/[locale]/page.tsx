25e34a270c30a95ca7ce5b81f82e71c145c669c8
Cristo RPS <cristobits0101@gmail.com>
2024-10-06 21:16:05 +0100

    Updating Styles

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,34 +8,34 @@ export default function HomePage() {
       <Header />
       <Sidebar />
       <main>
-        <section className='bg-slate-300 p-0 m-0'>
-          <h2 className='p-0 m-0'>Tendencias</h2>
-          <article className='p-0 m-0'>
-            <p className='p-0 m-0'>
+        <section>
+          <h2 className='text-lg font-semibold' >Tendencias</h2>
+          <article>
+            <p>
               Información sobre las tendencias actuales en series y películas.
             </p>
           </article>
         </section>
         <section>
-          <h2>Novedades</h2>
+          <h2 className='text-lg font-semibold' >Novedades</h2>
           <article>
             <p>Detalles sobre las últimas novedades en Netflix.</p>
           </article>
         </section>
         <section>
-          <h2>Recomendaciones</h2>
+          <h2 className='text-lg font-semibold' >Recomendaciones</h2>
           <article>
             <p>Recomendaciones personalizadas por género.</p>
           </article>
         </section>
         <section>
-          <h2>Suscripciones</h2>
+          <h2 className='text-lg font-semibold' >Suscripciones</h2>
           <article>
             <p>Detalles sobre los diferentes planes de suscripción.</p>
           </article>
         </section>
         <section>
-          <h2>Records</h2>
+          <h2 className='text-lg font-semibold' >Records</h2>
           <article>
             <p>Clasificaciones de contenido en Netflix.</p>
           </article>
