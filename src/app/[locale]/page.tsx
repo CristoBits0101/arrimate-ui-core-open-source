b5a6f57874561ac02d859002ab59350e15a92c44
Cristo RPS <cristobits0101@gmail.com>
2024-10-06 14:04:56 +0100

    Updating content

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,8 @@
 import '@/styles/globals.css'
 import Header from '@/layouts/header'
 import Sidebar from '@/layouts/sidebar'
-// import { useTranslations } from 'next-intl'
 
 export default function HomePage() {
-  // const t = useTranslations('SidebarLayout')
   return (
     <>
       <Header />
