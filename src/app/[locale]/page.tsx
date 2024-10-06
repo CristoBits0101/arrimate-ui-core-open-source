7184ca99935cd0d3cd6f48baf1b1409d52e57de0
Cristo RPS <cristobits0101@gmail.com>
2024-10-06 20:56:28 +0100

    Refactoring component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,11 +8,38 @@ export default function HomePage() {
       <Header />
       <Sidebar />
       <main>
-        <h2>Novedades</h2>
-        <h2>Tendencias</h2>
-        <h2>Recomendaciones</h2>
-        <h2>Suscripciones</h2>
-        <h2>Ranking</h2>
+        <section className='bg-slate-300 p-0 m-0'>
+          <h2 className='p-0 m-0'>Tendencias</h2>
+          <article className='p-0 m-0'>
+            <p className='p-0 m-0'>
+              Información sobre las tendencias actuales en series y películas.
+            </p>
+          </article>
+        </section>
+        <section>
+          <h2>Novedades</h2>
+          <article>
+            <p>Detalles sobre las últimas novedades en Netflix.</p>
+          </article>
+        </section>
+        <section>
+          <h2>Recomendaciones</h2>
+          <article>
+            <p>Recomendaciones personalizadas por género.</p>
+          </article>
+        </section>
+        <section>
+          <h2>Suscripciones</h2>
+          <article>
+            <p>Detalles sobre los diferentes planes de suscripción.</p>
+          </article>
+        </section>
+        <section>
+          <h2>Records</h2>
+          <article>
+            <p>Clasificaciones de contenido en Netflix.</p>
+          </article>
+        </section>
       </main>
     </>
   )
