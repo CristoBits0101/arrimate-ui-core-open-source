d26075c47f9f0069fc45f0aaf0efda72aa9087bb
Cristo RPS <cristobits0101@gmail.com>
2024-10-07 12:56:14 +0100

    Adding explore section

---
+++ src/components/feeds/explore.tsx
@@ -0,0 +1,24 @@
+'use client'
+import Image from 'next/image'
+import Link from 'next/link'
+import exploreBlackSVG from '@/assets/icons/sidebar/black/explore.svg'
+import exploreWhiteSVG from '@/assets/icons/sidebar/white/explore.svg'
+import { useActiveRoute } from '@/hooks/useActiveRoute'
+import { useLocale, useTranslations } from 'next-intl'
+
+export default function Explore() {
+  const isActive = useActiveRoute('explore')
+  const t = useTranslations('SidebarLayout')
+  return (
+    <li>
+      <Link href={`/${useLocale()}/explore`}>
+        {isActive ? (
+          <Image src={exploreBlackSVG} alt='Explore' />
+        ) : (
+          <Image src={exploreWhiteSVG} alt='Explore' />
+        )}
+        {t('explore')}
+      </Link>
+    </li>
+  )
+}
