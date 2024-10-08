60a5359f256e3b209d9b9bffa5cfdc217da0cd3d
Cristo RPS <cristobits0101@gmail.com>
2024-10-08 01:07:23 +0100

    Updating desing

---
+++ src/components/network/contacts.tsx
@@ -0,0 +1,14 @@
+import contactsBlackSVG from '@/assets/icons/sidebar/black/contacts.svg'
+import contactsWhiteSVG from '@/assets/icons/sidebar/white/contacts.svg'
+import NavigationItem from '@/components/navigation/navigation-item'
+
+export default function Contacts() {
+  return (
+    <NavigationItem
+      route='contacts'
+      blackIcon={contactsBlackSVG}
+      whiteIcon={contactsWhiteSVG}
+      textKey='contacts'
+    />
+  )
+}
