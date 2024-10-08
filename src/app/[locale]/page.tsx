636b8f94d8e66c9cb78a695e56b6c38d5bdb5dd9
Cristo RPS <cristobits0101@gmail.com>
2024-10-08 14:41:50 +0100

    Updating component name

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,47 +1,45 @@
+'use client'
+
 import '@/styles/globals.css'
 import Header from '@/layouts/header'
 import LeftSidebar from '@/layouts/left-sidebar'
 import RightSidebar from '@/layouts/right-sidebar'
-import { Carousel } from '@/components/ui/carousel'
+import CarouselHome from '@/components/feeds/carousel/carousel-home'
 
 export default function HomePage() {
   return (
     <>
       <Header />
       <LeftSidebar />
-      <main>
-        <Carousel />
-        News
+      <main className='flex flex-col gap-y-5 align-middle'>
         <section>
           <h2 className='text-lg font-semibold'>Tendencias</h2>
           <article>
-            <p>
-              Información sobre las tendencias actuales en series y películas.
-            </p>
+            <CarouselHome />
           </article>
         </section>
         <section>
           <h2 className='text-lg font-semibold'>Novedades</h2>
           <article>
-            <p>Detalles sobre las últimas novedades en Netflix.</p>
+            <CarouselHome />
           </article>
         </section>
         <section>
           <h2 className='text-lg font-semibold'>Recomendaciones</h2>
           <article>
-            <p>Recomendaciones personalizadas por género.</p>
+            <CarouselHome />
           </article>
         </section>
         <section>
           <h2 className='text-lg font-semibold'>Suscripciones</h2>
           <article>
-            <p>Detalles sobre los diferentes planes de suscripción.</p>
+            <CarouselHome />
           </article>
         </section>
         <section>
           <h2 className='text-lg font-semibold'>Records</h2>
           <article>
-            <p>Clasificaciones de contenido en Netflix.</p>
+            <CarouselHome />
           </article>
         </section>
       </main>
