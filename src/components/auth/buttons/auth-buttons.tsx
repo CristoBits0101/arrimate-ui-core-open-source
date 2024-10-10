989fb7540c4f3d3adfc24548fba8d47309996f80
Cristo RPS <cristobits0101@gmail.com>
2024-10-11 00:10:04 +0100

    Updating styles

--- src/components/auth/buttons/auth-buttons.tsx
+++ src/components/auth/buttons/auth-buttons.tsx
@@ -1,15 +1,27 @@
+import Image from 'next/image'
 import Link from 'next/link'
+import chevronRight from '@/assets/icons/arrow/chevron_right.svg'
 import { useLocale } from 'next-intl'
 
 export default function AuthButtons() {
   const locale = useLocale()
   return (
     <div className='flex w-full h-fit flex-row items-center justify-between'>
-      <button className='bg-[#F4F4F4] pr-8 pl-8 pt-2 pb-2 border-[0.094rem] border-[#bfbdc050] text-sm hover:bg-[#bfbdc050]'>
+      <button className='flex flex-row gap-1 bg-[#F4F4F4] pr-6 pl-6 pt-2 pb-2 border-[0.094rem] border-[#bfbdc050] text-sm hover:bg-[#bfbdc050]'>
         <Link href={`/${locale}/sign-in`}>Sign In</Link>
+        <Image
+          className='h-5 w-5'
+          src={chevronRight}
+          alt='Google'
+        />
       </button>
-      <button className='bg-[#F4F4F4] pr-8 pl-8 pt-2 pb-2 border-[0.094rem] border-[#bfbdc050] text-sm hover:bg-[#bfbdc050]'>
+      <button className='flex flex-row gap-1 bg-[#F4F4F4] pr-6 pl-6 pt-2 pb-2 border-[0.094rem] border-[#bfbdc050] text-sm hover:bg-[#bfbdc050]'>
         <Link href={`/${locale}/sign-up`}>Sign Up</Link>
+        <Image
+          className='h-5 w-5'
+          src={chevronRight}
+          alt='Google'
+        />
       </button>
     </div>
   )
