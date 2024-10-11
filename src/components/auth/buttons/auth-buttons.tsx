7df0d8c5b57d0d6006cfab30f9e173a9f23769cd
Cristo RPS <cristobits0101@gmail.com>
2024-10-11 10:39:40 +0100

    Updating style

--- src/components/auth/buttons/auth-buttons.tsx
+++ src/components/auth/buttons/auth-buttons.tsx
@@ -1,27 +1,28 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 import Link from 'next/link'
-import chevronRight from '@/assets/icons/arrow/chevron_right.svg'
-import { useLocale } from 'next-intl'
+// import chevronRight from '@/assets/icons/arrow/chevron_right.svg'
+import { useLocale, useTranslations } from 'next-intl'
 
 export default function AuthButtons() {
   const locale = useLocale()
+  const t = useTranslations('AuthButtons')
   return (
     <div className='flex w-full h-[2.5rem] flex-row items-center justify-between'>
-      <button className='flex flex-row justify-center gap-1 bg-[#F4F4F4] pr-6 pl-6 pt-2 pb-2 border-[0.094rem] border-[#bfbdc050] text-sm hover:bg-[#bfbdc050]'>
-        <Link href={`/${locale}/sign-in`}>Sign In</Link>
-        <Image
+      <button className='flex flex-row justify-center gap-1 bg-[#F4F4F4] pr-2 pl-2 pt-2 pb-2 border-[0.094rem] border-[#bfbdc050] text-sm hover:bg-[#bfbdc050]'>
+        <Link href={`/${locale}/sign-in`}>{t('signIn')}</Link>
+        {/* <Image
           className='h-5 w-5'
           src={chevronRight}
           alt='Google'
-        />
+        /> */}
       </button>
-      <button className='flex flex-row justify-center gap-1 bg-[#F4F4F4] pr-6 pl-6 pt-2 pb-2 border-[0.094rem] border-[#bfbdc050] text-sm hover:bg-[#bfbdc050]'>
-        <Link href={`/${locale}/sign-up`}>Sign Up</Link>
-        <Image
-          className='h-5 w-5'
+      <button className='flex flex-row justify-center gap-1 bg-[#F4F4F4] pr-2 pl-2 pt-2 pb-2 border-[0.094rem] border-[#bfbdc050] text-sm hover:bg-[#bfbdc050]'>
+        <Link href={`/${locale}/sign-up`}>{t('signUp')}</Link>
+        {/* <Image
+          className='h-5 w-5 flex items-start justify-center'
           src={chevronRight}
           alt='Google'
-        />
+        /> */}
       </button>
     </div>
   )
