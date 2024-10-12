8f010855cdcdecb77d0679c5f8bc4084a34e4522
Cristo RPS <cristobits0101@gmail.com>
2024-10-12 12:44:50 +0100

    Refactoring folder

--- src/components/navigation/menu/aside-sidebar.tsx
+++ src/components/navigation/menu/aside-sidebar.tsx
@@ -2,7 +2,7 @@ import Cart from '@/components/e-commerce/cart'
 import Messages from '@/components/messages/chats'
 import Notifications from '@/components/messages/notifications'
 import Post from '@/components/feeds/post'
-import Settings from '@/components/configuration/settings'
+import Settings from '@/components/configuration/link/settings'
 import styles from '@/styles/components/aside-sidebar.module.css'
 
 export default function Panel() {
