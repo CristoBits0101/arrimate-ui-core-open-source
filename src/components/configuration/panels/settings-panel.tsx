0e4740a0bd0f21cfe37d7fd6e8cab6fe05f1ec2b
Cristo RPS <cristobits0101@gmail.com>
2024-10-12 14:14:50 +0100

    Adding themes

--- src/components/configuration/panels/settings-panel.tsx
+++ src/components/configuration/panels/settings-panel.tsx
@@ -1,11 +1,13 @@
 'use client'
 
 import LanguageSelector from '@/components/configuration/languages/language-selector'
+import ModeSelector from '../themes/mode-selector'
 
 export default function SettingsPanel() {
   return (
     <div className='w-full h-full flex flex-col gap-8'>
       <LanguageSelector />
+      <ModeSelector />
     </div>
   )
 }
--- src/components/configuration/themes/mode-selector.tsx
+++ src/components/configuration/themes/mode-selector.tsx
@@ -5,11 +5,11 @@ export default function ModeSelector() {
   return (
     <section className='w-full h-fit flex flex-col gap-2'>
       <h2 className='flex flex-col justify-center font-medium'>
-        {t('language')}
+        {t('themes.title')}
       </h2>
       <select className='w-full h-fit pl-1 pr-1 pt-2 pb-2 border-[0.094rem] border-solid border-[#bfbdc050] outline-none bg-[#F4F4F4]'>
-        <option value='en'>{t('english')}</option>
-        <option value='es'>{t('spanish')}</option>
+        <option value='dark'>{t('themes.dark')}</option>
+        <option value='light'>{t('themes.light')}</option>
       </select>
     </section>
   )
