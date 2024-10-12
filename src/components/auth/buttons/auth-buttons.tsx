d5f623fd1f08f1054f63dfe258e25fb070938329
Cristo RPS <cristobits0101@gmail.com>
2024-10-12 11:18:49 +0100

    Updating styles

--- src/components/auth/buttons/auth-buttons.tsx
+++ src/components/auth/buttons/auth-buttons.tsx
@@ -7,17 +7,17 @@ export default function AuthButtons() {
   const locale = useLocale()
   const t = useTranslations('AuthButtons')
   return (
-    <div className='flex w-full h-[2.5rem] flex-row items-center justify-between'>
-      <button className='w-[7.25rem] h-[100%] rounded-full flex flex-row justify-center items-center gap-1 bg-[#F4F4F4] pr-4 pl-4 border-[0.094rem] border-[#bfbdc050] hover:bg-[#bfbdc050]'>
-        <Link className='w-full flex items-center justify-center h-fit' href={`/${locale}/sign-in`}>{t('signIn')}</Link>
+    <div className='flex w-full h-[2.5005rem] flex-row items-center justify-between'>
+      <button className='w-[7.25rem] flex flex-row justify-center gap-1 bg-[#F4F4F4] pr-4 pl-4 pt-2 pb-2 border-[0.094rem] border-[#bfbdc050] hover:bg-[#bfbdc050]'>
+        <Link className='w-full' href={`/${locale}/sign-in`}>{t('signIn')}</Link>
         {/* <Image
           className='h-5 w-5'
           src={chevronRight}
           alt='Google'
         /> */}
       </button>
-      <button className='w-[7.25rem] h-[100%] rounded-full flex flex-row justify-center items-center gap-1 bg-[#F4F4F4] pr-4 pl-4 border-[0.094rem] border-[#bfbdc050] hover:bg-[#bfbdc050]'>
-        <Link className='w-full flex items-center justify-center h-fit' href={`/${locale}/sign-up`}>{t('signUp')}</Link>
+      <button className='w-[7.25rem] flex flex-row justify-center gap-1 bg-[#F4F4F4] pr-4 pl-4 pt-2 pb-2 border-[0.094rem] border-[#bfbdc050] hover:bg-[#bfbdc050]'>
+        <Link className='w-full' href={`/${locale}/sign-up`}>{t('signUp')}</Link>
         {/* <Image
           className='h-5 w-5 flex items-start justify-center'
           src={chevronRight}
