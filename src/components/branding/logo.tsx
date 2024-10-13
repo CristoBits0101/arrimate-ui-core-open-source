c8b43c4950c37bb0d1783c59c3b6a5968deae6d4
Cristo RPS <cristobits0101@gmail.com>
2024-10-13 10:45:10 +0100

    Adding dark mode

--- src/components/branding/logo.tsx
+++ src/components/branding/logo.tsx
@@ -3,7 +3,7 @@ import { ds } from '@/lib/fonts'
 
 export default function Logo() {
   return (
-    <div className='text-4xl font-medium flex justify-center items-center'>
+    <div className='text-4xl font-medium flex justify-center items-center h-[2.5rem]'>
       <h1 className={ds.className}>Arrimate</h1>
     </div>
   )
--- src/components/configuration/themes/mode-selector.tsx
+++ src/components/configuration/themes/mode-selector.tsx
@@ -1,9 +1,73 @@
+'use client'
+
 import Image from 'next/image'
 import language from '@/assets/icons/settings/themes.svg'
+import { useEffect, useState } from 'react'
 import { useTranslations } from 'next-intl'
+
+// Allowed Themes
+type Theme = 'system' | 'dark' | 'light'
 
 export default function ModeSelector() {
   const t = useTranslations('SettingsPanel')
+  const [theme, setTheme] = useState<Theme>('system')
+
+  // Function to change the theme
+  const applyTheme = (selectedTheme: Theme): void => {
+    const root = document.documentElement
+    switch (selectedTheme) {
+      case 'dark':
+        root.setAttribute('data-mode', 'dark')
+        break
+      case 'light':
+        root.setAttribute('data-mode', 'light')
+        break
+      case 'system':
+        // Delete preferences
+        root.removeAttribute('data-mode')
+        // Apply system-based preferences
+        window.matchMedia('(prefers-color-scheme: dark)').matches
+          ? root.setAttribute('data-mode', 'dark')
+          : root.setAttribute('data-mode', 'light')
+    }
+  }
+
+  // Manejar el cambio de selección del usuario
+  const handleThemeChange = (e: React.ChangeEvent<HTMLSelectElement>): void => {
+    const selectedTheme = e.target.value as Theme
+    setTheme(selectedTheme)
+    selectedTheme === 'system'
+      ? localStorage.removeItem('theme')
+      : localStorage.setItem('theme', selectedTheme)
+    applyTheme(selectedTheme)
+  }
+
+  // Configurar el tema inicial al cargar la página
+  useEffect(() => {
+    // Verificar si el entorno es el navegador
+    if (typeof window !== 'undefined') {
+      const storedTheme = localStorage.getItem('theme') as Theme | null
+      if (storedTheme) {
+        setTheme(storedTheme)
+        applyTheme(storedTheme)
+      } else {
+        setTheme('system')
+        applyTheme('system')
+      }
+
+      // Escuchar cambios en las preferencias de color del sistema
+      const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+      const handleSystemThemeChange = (): void => {
+        if (!localStorage.getItem('theme')) applyTheme('system')
+      }
+
+      mediaQuery.addEventListener('change', handleSystemThemeChange)
+
+      // Clear effect
+      return () => mediaQuery.removeEventListener('change', handleSystemThemeChange)
+    }
+  }, [])
+
   return (
     <section className='text-sm w-full h-fit flex flex-col gap-2'>
       <div className='w-full h-fit flex flex-row items-center font-medium gap-2'>
@@ -12,7 +76,12 @@ export default function ModeSelector() {
           {t('themes.title')}
         </h2>
       </div>
-      <select className='rounded-lg font-normal w-full h-fit pl-1 pr-1 pt-2 pb-2 border-[0.05rem] border-solid border-[#bfbdc050] outline-none bg-[#F4F4F4]'>
+      <select
+        className='rounded-lg font-normal w-full h-fit pl-1 pr-1 pt-2 pb-2 border-[0.05rem] border-solid border-[#bfbdc050] outline-none bg-[#F4F4F4]'
+        onChange={handleThemeChange}
+        value={theme}
+      >
+        <option value='system'>{t('themes.system')}</option>
         <option value='dark'>{t('themes.dark')}</option>
         <option value='light'>{t('themes.light')}</option>
       </select>
