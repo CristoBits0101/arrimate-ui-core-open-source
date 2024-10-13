069c508ce87454b21bbc815bfbe6248271424bb6
Cristo RPS <cristobits0101@gmail.com>
2024-10-13 18:51:23 +0100

    Refactoring files

--- src/components/navigation/menu/navbar-menu.tsx
+++ src/components/navigation/menu/navbar-menu.tsx
@@ -1,9 +1,9 @@
 import Community from '@/components/network/links/community-link'
 import Events from '@/components/feeds/links/events-link'
-import Home from '@/components/feeds/links/home-link.'
+import Home from '@/components/feeds/links/home-link'
 import Live from '@/components/streaming/links/live'
 import Products from '@/components/e-commerce/links/products-link'
-import Reviews from '@/components/feeds/links/reviews-link.'
+import Reviews from '@/components/feeds/links/reviews-link'
 import Shorts from '@/components/streaming/links/shorts'
 import Videos from '@/components/streaming/links/videos'
 
--- src/components/navigation/menu/sidebar-menu.tsx
+++ src/components/navigation/menu/sidebar-menu.tsx
@@ -1,15 +1,15 @@
-import Cart from '@/components/e-commerce/links/cart-link'
-import Messages from '@/components/messages/links/chats-link.'
-import Notifications from '@/components/messages/links/notifications-link.'
-import Post from '@/components/feeds/links/post-link.'
 import Settings from '@/components/configuration/links/settings'
+import Cart from '@/components/e-commerce/links/cart-link'
+import Post from '@/components/feeds/links/post-link'
+import Chats from '@/components/messages/links/chats-link'
+import Notifications from '@/components/messages/links/notifications-link'
 
 export default function SidebarMenu() {
   return (
     <nav className='flex w-full items-center h-fit'>
       <ul className='flex items-center flex-row justify-between w-full h-11'>
         <Post />
-        <Messages />
+        <Chats />
         <Notifications />
         <Cart />
         <Settings />
