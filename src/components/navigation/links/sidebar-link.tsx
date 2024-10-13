59959329629529d42799ad776c9ff436c86ed8ae
Cristo RPS <cristobits0101@gmail.com>
2024-10-13 21:24:09 +0100

    updating style

--- src/components/navigation/links/sidebar-link.tsx
+++ src/components/navigation/links/sidebar-link.tsx
@@ -24,7 +24,7 @@ export default function SidebarItem({
   const href = route === 'home' ? `/${locale}` : `/${locale}/${route}`
 
   return (
-    <li className='flex items-center justify-center w-fit h-fit pt-2 pb-2 hover:cursor-pointer bg-[#F0F0FE]'>
+    <li className='flex items-center justify-center w-fit h-fit pt-2 pb-2 hover:cursor-pointer'>
       <Link
         className='flex items-center justify-center h-fit w-fit gap-4'
         href={href}
