61a629ca090ea52ab4069cc9d64b35a7bc429ae7
Cristo RPS <cristobits0101@gmail.com>
2024-10-13 18:07:20 +0100

    Updating styles

--- src/components/navigation/menu/aside-sidebar.tsx
+++ src/components/navigation/menu/aside-sidebar.tsx
@@ -3,12 +3,11 @@ import Messages from '@/components/messages/links/chats-link.'
 import Notifications from '@/components/messages/links/notifications-link.'
 import Post from '@/components/feeds/links/post-link.'
 import Settings from '@/components/configuration/links/settings'
-import styles from '@/styles/components/aside-sidebar.module.css'
 
 export default function Panel() {
   return (
-    <nav className={styles.aside}>
-      <ul className='flex flex-row justify-between'>
+    <nav className='flex w-full items-center h-fit'>
+      <ul className='flex flex-row justify-between w-full h-[2.875rem]'>
         <Post />
         <Messages />
         <Notifications />
