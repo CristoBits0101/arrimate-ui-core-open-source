a83f264bc3a9a69124a1653e3d158378b7b6c0c7
Cristo RPS <cristobits0101@gmail.com>
2024-10-14 13:58:39 +0100

    Updating code

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import CarouselHome from '@/components/feeds/carousels/carousel-home'
+import StoriesCarousel from '@/components/feeds/carousels/stories-carousel'
 import Header from '@/layouts/header'
 import Aside from '@/layouts/aside'
 import '@/styles/globals.css'
@@ -10,7 +10,7 @@ export default function HomePage() {
     <>
       <Header />
       <main className='col-span-1'>
-        <CarouselHome />
+        <StoriesCarousel />
       </main>
       <Aside />
     </>
