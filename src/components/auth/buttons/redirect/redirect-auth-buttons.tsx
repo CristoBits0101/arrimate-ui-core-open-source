122075f07f733ad3b0ef14490432eda16952b674
Cristo RPS <cristobits0101@gmail.com>
2024-10-17 20:14:53 +0100

    Updating styles

--- src/components/auth/buttons/redirect/redirect-auth-buttons.tsx
+++ src/components/auth/buttons/redirect/redirect-auth-buttons.tsx
@@ -6,12 +6,15 @@ export default function AuthButtons() {
   const t = useTranslations('AuthButtons')
   return (
     <div className='flex w-full h-[2.5rem] flex-row items-center justify-between'>
-      <button className='font-medium w-[7.25rem] flex flex-row justify-center gap-1 bg-[#F4F4F4] pr-4 pl-4 pt-2 pb-2 border-[0.05rem] border-[#bfbdc050] border-solid hover:bg-[#bfbdc050] h-full'>
-        <Link className='w-full h-fit' href={`/${locale}/sign-in`}>{t('signIn')}</Link>
-        
+      <button className='font-medium w-[7.25rem] flex flex-row justify-center gap-1 bg-[#F4F4F4] p-2 border-[0.05rem] border-[#bfbdc050] border-solid hover:bg-[#bfbdc050] h-full'>
+        <Link className='w-full h-fit flex justify-center' href={`/${locale}/sign-in`}>
+          {t('signIn')} 
+        </Link>
       </button>
-      <button className='font-medium w-[7.25rem] flex flex-row justify-center gap-1 bg-[#F4F4F4] pr-4 pl-4 pt-2 pb-2 border-[0.05rem] border-[#bfbdc050] border-solid hover:bg-[#bfbdc050] h-full'>
-        <Link className='w-full h-fit' href={`/${locale}/sign-up`}>{t('signUp')}</Link>
+      <button className='font-medium w-[7.25rem] flex flex-row justify-center gap-1 bg-[#F4F4F4] p-2 border-[0.05rem] border-[#bfbdc050] border-solid hover:bg-[#bfbdc050] h-full'>
+        <Link className='w-full h-fit flex justify-center' href={`/${locale}/sign-up`}>
+          {t('signUp')}
+        </Link>
       </button>
     </div>
   )
