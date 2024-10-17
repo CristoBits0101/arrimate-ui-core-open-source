88f61bedfc4a53260f654d31344ed6cc3cff6bda
Cristo RPS <cristobits0101@gmail.com>
2024-10-17 23:28:18 +0100

    Actualizando diseño de las opciones

--- src/components/configuration/panels/settings-panel.tsx
+++ src/components/configuration/panels/settings-panel.tsx
@@ -1,33 +1,13 @@
 'use client'
 
-import Image from 'next/image'
 import LanguageSelector from '@/components/configuration/options/language-selector'
 import ThemeSelector from '@/components/configuration/options/theme-selector'
-import password from '@/assets/icons/settings/password.svg'
 
 export default function SettingsPanel() {
   return (
-    <section className='w-full h-full flex flex-col gap-6 p-6 rounded-3xl border-[0.05rem] border-[#bfbdc050] border-solid'>
-      {/*  */}
+    <div className='w-full h-full flex flex-col gap-8 p-8 rounded-3xl border-[0.05rem] border-[#bfbdc050] border-solid'>
       <LanguageSelector />
-      {/*  */}
       <ThemeSelector />
-      {/*  */}
-      <details name='option' className='group'>
-        <summary className='text-sm font-medium flex justify-between items-center cursor-pointer mb-2'>
-          <h2 className='flex gap-2'>
-            <Image
-              src={password}
-              alt='Languages'
-              className='w-5 h-5 aspect-square'
-            />
-            Password
-          </h2>
-        </summary>
-        <article>
-          <p>Theme options...</p>
-        </article>
-      </details>
-    </section>
+    </div>
   )
 }
