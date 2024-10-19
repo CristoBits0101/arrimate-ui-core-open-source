6610cbcebbffef15e1f6e40b4e163d46bb12290e
Cristo RPS <cristobits0101@gmail.com>
2024-10-19 18:18:16 +0100

    formating sintax

--- src/components/feeds/post/show-images.tsx
+++ src/components/feeds/post/show-images.tsx
@@ -1,18 +1,30 @@
-import Image from "next/image"
+import Image from 'next/image'
 
 export default function ShowImages() {
   return (
     <section className='w-full h-full flex flex-col justify-center items-center gap-6'>
       <article className='rounded-3xl w-[25vw] h-[75vh] bg-blue-200 flex justify-center items-center'>
-        <Image className='w-full h-full aspect-video object-cover' src='' alt='1' /> 
+        <Image
+          className='w-full h-full aspect-video object-cover'
+          src=''
+          alt='1'
+        />
       </article>
       <hr />
       <article className='rounded-3xl w-[25vw] h-[75vh] bg-blue-200 flex justify-center items-center'>
-        <Image className='w-full h-full aspect-video object-cover' src=''alt='2'  />
+        <Image
+          className='w-full h-full aspect-video object-cover'
+          src=''
+          alt='2'
+        />
       </article>
       <hr />
       <article className='rounded-3xl w-[25vw] h-[75vh] bg-blue-200 flex justify-center items-center'>
-        <Image className='w-full h-full aspect-video object-cover' src=''alt='3'  />
+        <Image
+          className='w-full h-full aspect-video object-cover'
+          src=''
+          alt='3'
+        />
       </article>
     </section>
   )
