4df6c0e9571b4965849ee6a9b99994e98f33cf60
Cristo RPS <cristobits0101@gmail.com>
2024-10-20 19:30:55 +0100

    Updating text

--- src/components/branding/application-logo.tsx
+++ src/components/branding/application-logo.tsx
@@ -4,7 +4,7 @@ import { ds } from '@/lib/fonts'
 export default function Logo() {
   return (
     <div className='text-4xl font-medium flex justify-center items-center w-full h-[2.5rem] dark:text-[#f1f3f4]'>
-      <h1 className={ds.className}>Arrimate</h1>
+      <h1 className={ds.className}>Arrímate</h1>
     </div>
   )
 }
