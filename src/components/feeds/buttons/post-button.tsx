8758de2865bf50c683d9287e2ae79b3bd85179de
Cristo RPS <cristobits0101@gmail.com>
2024-10-25 15:51:01 +0100

    Refactoring buttons

--- src/components/feeds/buttons/post-button.tsx
+++ src/components/feeds/buttons/post-button.tsx
@@ -1,6 +1,6 @@
 import { GoHeartFill } from 'react-icons/go'
 import { FaCommentDots } from 'react-icons/fa'
-import { BsBookmarkFill } from 'react-icons/bs'
+import { HiBookmark } from 'react-icons/hi2'
 import { PiShareFatFill } from 'react-icons/pi'
 import { SlOptionsVertical } from 'react-icons/sl'
 
@@ -17,13 +17,13 @@ export default function PostButton({
   textDisplay = '',
   textColor = '#FFFFFF',
   iconDisplay = '',
-  iconColor,
+  iconColor = '#FFFFFF',
   iconAlt = '',
   iconSize = 24
 }: PostButtonProps) {
   return (
     <div className='w-fit h-fit flex flex-col gap-2 justify-center items-center'>
-      <button className='w-fit h-fit p-2 rounded-full'>
+      <button className='w-fit h-fit p-2 rounded-full bg-red-400'>
         {iconDisplay === 'like' && (
           <GoHeartFill size={iconSize} color={iconColor} aria-label={iconAlt} />
         )}
@@ -42,11 +42,7 @@ export default function PostButton({
           />
         )}
         {iconDisplay === 'share' && (
-          <BsBookmarkFill
-            size={iconSize}
-            color={iconColor}
-            aria-label={iconAlt}
-          />
+          <HiBookmark size={iconSize} color={iconColor} aria-label={iconAlt} />
         )}
         {iconDisplay === 'options' && (
           <SlOptionsVertical
