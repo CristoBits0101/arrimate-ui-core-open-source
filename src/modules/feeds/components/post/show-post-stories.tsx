bd0c089b27b401536554da2f99ef0fefb5cf6fba
Cristo RPS <cristobits0101@gmail.com>
2024-11-10 16:38:36 +0000

    Refactoring component

--- src/modules/feeds/components/post/show-post-stories.tsx
+++ src/modules/feeds/components/post/show-post-stories.tsx
@@ -1,5 +1,16 @@
-import HomeCarousel from '@/modules/feeds/components/carousels/home-carousel'
+'use client'
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination } from 'swiper/modules'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
 import { useFetchPhotos } from '@/modules/feeds/hooks/useFetchPhotos'
+import unknownImage from '@/modules/feeds/assets/images/profile/aspect-ratio-1-1/unknownImage.jpg'
 
 export default function ShowPostStories() {
   const { photos, loading, error } = useFetchPhotos({
@@ -11,6 +22,33 @@ export default function ShowPostStories() {
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error: {error}</p>
-  
-  return <HomeCarousel photos={photos} />
+
+  return (
+    <Swiper
+      modules={[Navigation, Pagination]}
+      spaceBetween={10}
+      slidesPerView={5}
+      navigation
+      pagination={{ clickable: true }}
+    >
+      {photos.map((photo) => (
+        <SwiperSlide key={photo.id}>
+          <div className='relative w-20 h-20 flex flex-col text-center gap-2'>
+            <span
+              className='relative w-20 h-20 flex items-center justify-center bg-cover bg-center bg-no-repeat aspect-square rounded-full shadow-sm'
+              style={{
+                backgroundColor: photo.avg_color || '#f0f0f0',
+                backgroundImage: `url(${photo.src.small || unknownImage})`
+              }}
+            ></span>
+            <h3 className='w-20 text-sm'>
+              {photo.photographer.length > 8
+                ? photo.photographer.slice(0, 7) + '...'
+                : photo.photographer}
+            </h3>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  )
 }
