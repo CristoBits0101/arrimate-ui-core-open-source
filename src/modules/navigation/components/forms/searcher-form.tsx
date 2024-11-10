0bc15a973d50ca1f430eab7f6e9df9efb4104e1b
Cristo RPS <cristobits0101@gmail.com>
2024-11-10 15:30:16 +0000

    Refactoring

--- src/modules/navigation/components/forms/searcher-form.tsx
+++ src/modules/navigation/components/forms/searcher-form.tsx
@@ -37,9 +37,9 @@ export default function Searcher({
   return (
     <form
       onSubmit={handleSubmit}
-      className='form flex items-center justify-center text-[#1d0f0f] text-[0.875rem] w-full h-11'
+      className='flex items-center justify-center text-[#1d0f0f] text-[0.875rem] w-full h-11'
     >
-      <div>
+      <div className='flex items-center bg-[#f4f4f4] border border-[rgba(191,189,192,0.31)] rounded-full h-full w-full'>
         <input
           className='appearance-[textfield] bg-transparent border-0 border-r-[0.05rem] border-r-[#bfbdc050] h-1/2 outline-none p-4 w-full border-solid'
           placeholder={t('placeholder')}
