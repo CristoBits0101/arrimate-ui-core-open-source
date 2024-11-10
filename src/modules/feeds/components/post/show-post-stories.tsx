db1c79e998a4269642804a0c1ed33f235854bb40
Cristo RPS <cristobits0101@gmail.com>
2024-11-10 17:31:29 +0000

    Updating logic

--- src/modules/feeds/components/post/show-post-stories.tsx
+++ src/modules/feeds/components/post/show-post-stories.tsx
@@ -1,54 +1,82 @@
-'use client'
-
-// Import Swiper React components
+import Image from 'next/image'
+import { Navigation } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, Pagination } from 'swiper/modules'
+import { useFetchPhotos } from '@/modules/feeds/hooks/useFetchPhotos'
+import { useState, useEffect, useRef, useCallback } from 'react'
 
 // Import Swiper styles
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 
-import { useFetchPhotos } from '@/modules/feeds/hooks/useFetchPhotos'
-import unknownImage from '@/modules/feeds/assets/images/profile/aspect-ratio-1-1/unknownImage.jpg'
-
 export default function ShowPostStories() {
   const { photos, loading, error } = useFetchPhotos({
     query: 'personas',
     orientation: 'square',
-    per_page: 84,
+    per_page: 30,
     page: 4
   })
+
+  const [slidesPerView, setSlidesPerView] = useState(1)
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  const updateSlidesPerView = useCallback(() => {
+    if (containerRef.current) {
+      const containerWidth = containerRef.current.offsetWidth
+      const newSlidesPerView = Math.floor(containerWidth / 96)
+      setSlidesPerView(newSlidesPerView)
+    }
+  }, [])
+
+  useEffect(() => {
+    updateSlidesPerView()
+    window.addEventListener('resize', updateSlidesPerView)
+    return () => window.removeEventListener('resize', updateSlidesPerView)
+  }, [updateSlidesPerView])
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error: {error}</p>
 
+  // Create a set of unique photos based on the src
+  const uniquePhotos = Array.from(
+    new Set(photos.map((photo) => photo.src?.small))
+  ).map((src) => photos.find((photo) => photo.src?.small === src))
+
   return (
-    <Swiper
-      modules={[Navigation, Pagination]}
-      spaceBetween={10}
-      slidesPerView={5}
-      navigation
-      pagination={{ clickable: true }}
+    <div
+      ref={containerRef}
+      className='relative w-full flex justify-between items-center'
     >
-      {photos.map((photo) => (
-        <SwiperSlide key={photo.id}>
-          <div className='relative w-20 h-20 flex flex-col text-center gap-2'>
-            <span
-              className='relative w-20 h-20 flex items-center justify-center bg-cover bg-center bg-no-repeat aspect-square rounded-full shadow-sm'
-              style={{
-                backgroundColor: photo.avg_color || '#f0f0f0',
-                backgroundImage: `url(${photo.src.small || unknownImage})`
-              }}
-            ></span>
-            <h3 className='w-20 text-sm'>
-              {photo.photographer.length > 8
-                ? photo.photographer.slice(0, 7) + '...'
-                : photo.photographer}
-            </h3>
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+      <Swiper
+        modules={[Navigation]}
+        spaceBetween={15}
+        slidesPerView={slidesPerView}
+        navigation
+        pagination={{ clickable: true }}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        className='relative w-full flex justify-between items-center'
+      >
+        {uniquePhotos.map(
+          (photo) =>
+            photo &&
+            photo.src && (
+              <SwiperSlide key={photo.id}>
+                <div className='w-20 h-20 overflow-hidden flex items-center justify-center rounded-full'>
+                  <Image
+                    src={photo.src.small}
+                    alt={photo.alt || 'Image from Pexels'}
+                    width={photo.width}
+                    height={photo.height}
+                    layout='responsive'
+                    objectFit='cover'
+                    className='rounded-full drop-shadow-sm border-solid border-[0.05rem] border-[#bfbdc050]'
+                  />
+                </div>
+              </SwiperSlide>
+            )
+        )}
+      </Swiper>
+    </div>
   )
 }
