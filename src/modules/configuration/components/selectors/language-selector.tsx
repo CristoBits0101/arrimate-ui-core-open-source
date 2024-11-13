4743574d89f78a88443d0daa2f62e07715dd7a21
Cristo RPS <cristobits0101@gmail.com>
2024-11-13 12:13:44 +0000

    Updating icons

--- src/modules/configuration/components/selectors/language-selector.tsx
+++ src/modules/configuration/components/selectors/language-selector.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+import Image from 'next/image'
+import en from '@/modules/configuration/assets/icons/selectors/gb.svg'
+import es from '@/modules/configuration/assets/icons/selectors/es.svg'
 import { useRouter, usePathname } from '@/i18n/routing'
 import { useTranslations, useLocale } from 'next-intl'
 import { useEffect } from 'react'
@@ -12,7 +15,7 @@ const LanguageSelector = () => {
   const supportedLocales: Array<'en' | 'es'> = ['en', 'es']
 
   useEffect(() => {
-    // 
+    //
     if (!localStorage.getItem('language'))
       localStorage.setItem('language', locale)
   }, [locale])
@@ -29,20 +32,34 @@ const LanguageSelector = () => {
   return (
     <section className='w-full h-fit flex flex-col'>
       <button
-        className={`text-left cursor-pointer px-8 py-2 hover:bg-[#F4F4F4] ${
+        className={`text-left cursor-pointer px-8 py-2 hover:bg-[#F4F4F4] flex justify-between items-center gap-2 ${
           locale === 'en' ? 'bg-[#F4F4F4] font-medium' : ''
         }`}
         onClick={() => handleLanguageClick('en')}
       >
         {t('language.english')}
+        <Image
+          width={24}
+          height={18}
+          src={en}
+          alt='English icon'
+          className='object-cover'
+        />
       </button>
       <button
-        className={`text-left cursor-pointer px-8 py-2 hover:bg-[#F4F4F4] ${
+        className={`text-left cursor-pointer px-8 py-2 hover:bg-[#F4F4F4] flex justify-between items-center gap-2 ${
           locale === 'es' ? 'bg-[#F4F4F4] font-medium' : ''
         }`}
         onClick={() => handleLanguageClick('es')}
       >
         {t('language.spanish')}
+        <Image
+          width={24}
+          height={18}
+          src={es}
+          alt='Spanish icon'
+          className='object-cover'
+        />
       </button>
     </section>
   )
