431b73f9eb42b0ac30d3e9e13c07d9c89bc67625
Cristo RPS <cristobits0101@gmail.com>
2024-11-13 17:13:37 +0000

    Refactoring size

--- src/modules/configuration/components/buttons/back-button.tsx
+++ src/modules/configuration/components/buttons/back-button.tsx
@@ -13,12 +13,12 @@ const BackButton: React.FC<BackButtonProps> = ({ onClick }) => {
 
   return (
     <button
-      className='w-7 absolute left-8 top-1/2 transform -translate-y-1/2 flex items-center bg-[#F4F4F4] aspect-square rounded-full border-[0.05rem] border-[#EBEAEB] border-solid'
+      className='w-6 absolute left-8 top-1/2 transform -translate-y-1/2 flex items-center bg-[#F4F4F4] aspect-square rounded-full border-[0.05rem] border-[#EBEAEB] border-solid'
       onClick={onClick}
     >
       <Image
-        width={28}
-        height={28}
+        width={24}
+        height={24}
         src={backIcon}
         alt={t('backIconAlt', { defaultMessage: 'Back' })}
         className='aspect-square object-contain'
