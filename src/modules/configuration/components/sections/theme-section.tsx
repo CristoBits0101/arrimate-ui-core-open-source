0d60ec363642412a63ace9da552c83c4d7fe7131
Cristo RPS <cristobits0101@gmail.com>
2024-11-14 23:01:06 +0000

    Updating logic

--- src/modules/configuration/components/sections/theme-section.tsx
+++ src/modules/configuration/components/sections/theme-section.tsx
@@ -10,35 +10,35 @@ import inactiveSystemIcon from '@/modules/configuration/assets/icons/buttons/the
 import useThemeSection from '@/modules/configuration/hooks/useThemeSection'
 import { useTranslations } from 'next-intl'
 
-const ThemeSelector = () => {
+const ThemeSection = () => {
   const { theme, changeTheme } = useThemeSection()
   const t = useTranslations('SettingsPanel')
 
   return (
-    <section className='w-full h-fit flex flex-col'>
+    <section className="w-full h-fit flex flex-col">
       <OptionButton
         label={t('themes.dark')}
         isSelected={theme === 'dark'}
         onClick={() => changeTheme('dark')}
         iconSrc={theme === 'dark' ? activeDarkIcon : inactiveDarkIcon}
-        altText='Dark theme icon'
+        altText="Dark theme icon"
       />
       <OptionButton
         label={t('themes.light')}
         isSelected={theme === 'light'}
         onClick={() => changeTheme('light')}
         iconSrc={theme === 'light' ? activeLightIcon : inactiveLightIcon}
-        altText='Light theme icon'
+        altText="Light theme icon"
       />
       <OptionButton
         label={t('themes.system')}
         isSelected={theme === 'system'}
         onClick={() => changeTheme('system')}
         iconSrc={theme === 'system' ? activeSystemIcon : inactiveSystemIcon}
-        altText='System theme icon'
+        altText="System theme icon"
       />
     </section>
   )
 }
 
-export default ThemeSelector
+export default ThemeSection
