a598bf813fa8d704437f2905789025319cb0f8f7
Cristo RPS <cristobits0101@gmail.com>
2024-11-14 22:33:24 +0000

    Updating theme styles

--- src/modules/configuration/components/sections/theme-section.tsx
+++ src/modules/configuration/components/sections/theme-section.tsx
@@ -1,9 +1,12 @@
 'use client'
 
 import OptionButton from '@/modules/configuration/components/buttons/option-button'
-import darkIcon from '@/modules/configuration/assets/icons/buttons/themes/inactive/dark.svg'
-import lightIcon from '@/modules/configuration/assets/icons/buttons/themes/inactive/light.svg'
-import systemIcon from '@/modules/configuration/assets/icons/buttons/themes/inactive/system.svg'
+import activeDarkIcon from '@/modules/configuration/assets/icons/buttons/themes/active/dark.svg'
+import activeLightIcon from '@/modules/configuration/assets/icons/buttons/themes/active/light.svg'
+import activeSystemIcon from '@/modules/configuration/assets/icons/buttons/themes/active/system.svg'
+import inactiveDarkIcon from '@/modules/configuration/assets/icons/buttons/themes/inactive/dark.svg'
+import inactiveLightIcon from '@/modules/configuration/assets/icons/buttons/themes/inactive/light.svg'
+import inactiveSystemIcon from '@/modules/configuration/assets/icons/buttons/themes/inactive/system.svg'
 import useThemeSection from '@/modules/configuration/hooks/useThemeSection'
 import { useTranslations } from 'next-intl'
 
@@ -12,27 +15,27 @@ const ThemeSelector = () => {
   const t = useTranslations('SettingsPanel')
 
   return (
-    <section className="w-full h-fit flex flex-col">
+    <section className='w-full h-fit flex flex-col'>
       <OptionButton
         label={t('themes.dark')}
         isSelected={theme === 'dark'}
         onClick={() => changeTheme('dark')}
-        iconSrc={darkIcon}
-        altText="Dark theme icon"
+        iconSrc={theme === 'dark' ? activeDarkIcon : inactiveDarkIcon}
+        altText='Dark theme icon'
       />
       <OptionButton
         label={t('themes.light')}
         isSelected={theme === 'light'}
         onClick={() => changeTheme('light')}
-        iconSrc={lightIcon}
-        altText="Light theme icon"
+        iconSrc={theme === 'light' ? activeLightIcon : inactiveLightIcon}
+        altText='Light theme icon'
       />
       <OptionButton
         label={t('themes.system')}
         isSelected={theme === 'system'}
         onClick={() => changeTheme('system')}
-        iconSrc={systemIcon}
-        altText="System theme icon"
+        iconSrc={theme === 'system' ? activeSystemIcon : inactiveSystemIcon}
+        altText='System theme icon'
       />
     </section>
   )
