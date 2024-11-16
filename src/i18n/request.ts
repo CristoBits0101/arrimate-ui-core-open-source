099458c0de151d6058e81f958d23ce6e1ca321cd
Cristo RPS <cristobits0101@gmail.com>
2024-11-16 17:07:56 +0000

    Updating code

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,10 +1,17 @@
-import { notFound } from 'next/navigation'
 import { getRequestConfig } from 'next-intl/server'
 import { routing } from '@/i18n/routing'
 
-export default getRequestConfig(async ({ locale }) => {
-  if (!routing.locales.includes(locale as any)) notFound()
+export default getRequestConfig(async ({ requestLocale }) => {
+  let locale = await requestLocale
+
+  if (
+    !locale ||
+    !routing.locales.includes(locale as (typeof routing)['locales'][number])
+  )
+    locale = routing.defaultLocale
+
   return {
+    locale: locale as (typeof routing)['locales'][number],
     messages: (await import(`../../messages/${locale}.json`)).default
   }
 })
--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,10 +1,10 @@
 import { defineRouting } from 'next-intl/routing'
-import { createSharedPathnamesNavigation } from 'next-intl/navigation'
+import { createNavigation } from 'next-intl/navigation'
 
 export const routing = defineRouting({
   locales: ['en', 'es'],
   defaultLocale: 'en'
 })
 
-export const { Link, redirect, usePathname, useRouter } =
-  createSharedPathnamesNavigation(routing)
+export const { Link, redirect, usePathname, useRouter } = createNavigation(routing)
+export type Locale = (typeof routing)['locales'][number]
