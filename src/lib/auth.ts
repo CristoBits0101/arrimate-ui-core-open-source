8ba9a48668e0bd080fe1d3fddba5c5092df33073
Cristo RPS <cristobits0101@gmail.com>
2024-11-17 17:53:05 +0000

    Adding google provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,17 @@
 import NextAuth from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     GitHubProvider({
       clientId: process.env.AUTH_GITHUB_ID,
       clientSecret: process.env.AUTH_GITHUB_SECRET
+    }),
+    GoogleProvider({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET
     })
   ],
-  secret: process.env.AUTH_SECRET,
-  debug: true
+  secret: process.env.AUTH_SECRET
 })
