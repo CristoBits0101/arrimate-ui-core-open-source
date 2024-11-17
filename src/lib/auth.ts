4f53ff0cff83f3c2653dc70c1789199453a8dbe9
Cristo RPS <cristobits0101@gmail.com>
2024-11-17 15:27:27 +0000

    Updating config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,12 @@
 import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import GitHubProvider from 'next-auth/providers/github'
 
-export const { auth, handlers, signIn, signOut } = NextAuth({
+export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-    GoogleProvider({
-      clientId: process.env.AUTH_GOOGLE_ID!,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET!
+    GitHubProvider({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET
     })
-  ]
+  ],
+  debug: true
 })
