16ca93a89a7af74b75d591c226b886b953f0364c
Cristo RPS <cristobits0101@gmail.com>
2024-11-19 13:06:32 +0000

    Adding default image

--- src/modules/auth/components/cards/user-card.tsx
+++ src/modules/auth/components/cards/user-card.tsx
@@ -1,16 +1,24 @@
+import defaultUserImage from '@/modules/auth/assets/images/default_user_image.png'
+import Image from 'next/image'
 // import { auth } from '@/lib/auth'
 
 export default function UserCard() {
-    // const session = await auth()
-    return (
-        <article className='flex w-full h-10 gap-4 text-sm'>
-          <figure className='w-10 h-full flex items-center justify-center'>
-            Photo
-          </figure>
-          <section className='w-full h-full flex flex-col items-start justify-center'>
-            {/* <p className=''>{session?.user?.email ?? ''}</p> */}
-            <p>CristoBits0101</p>
-          </section>
-        </article>
-      )
+  // const session = await auth()
+  return (
+    <article className='flex w-full h-12 gap-4'>
+      <figure className='w-10 h-full flex items-center justify-center'>
+        <Image
+          src={defaultUserImage ?? 'Photo'}
+          alt='User profile picture'
+          width={40}
+          height={40}
+          className='aspect-square object-cover drop-shadow-sm rounded-full'
+        />
+      </figure>
+      <section className='w-full h-full flex flex-col items-start justify-center'>
+        {/* <p className=''>{session?.user?.email ?? ''}</p> */}
+        <p>CristoBits0101</p>
+      </section>
+    </article>
+  )
 }
