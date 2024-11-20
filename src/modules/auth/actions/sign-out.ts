3d611d689683d3c70febab38c549defabec666fa
Cristo RPS <cristobits0101@gmail.com>
2024-11-20 14:59:02 +0000

    Updating dependencies

---
+++ src/modules/auth/actions/sign-out.ts
@@ -0,0 +1,8 @@
+'use server'
+
+import { signOut } from '@/lib/auth'
+
+export async function signOutAction() {
+  await signOut()
+  console.log('User signed out')
+}
--- src/modules/auth/components/buttons/sign-out/sign-out-button.tsx
+++ src/modules/auth/components/buttons/sign-out/sign-out-button.tsx
@@ -1,19 +1,11 @@
-import { Button } from '@/modules/ui/button'
-import { signOut } from '@/lib/auth'
-import { useTranslations } from 'next-intl'
+'use client'
 
-export default function SignOutButton() {
-  const t = useTranslations('Button')
+import { signOutAction } from '@/modules/auth/actions/sign-out'
 
-  const handleSignOut = async (e: React.FormEvent<HTMLFormElement>) => {
-    'use server'
-    e.preventDefault() // Prevenir el comportamiento por defecto del formulario.
-    await signOut() // Ejecutar la función de cierre de sesión.
-  }
-
+export function SignOutButton() {
   return (
-    <form onSubmit={handleSignOut}>
-      <Button type='submit'>{t('SignOut')}</Button>
+    <form action={signOutAction}>
+      <button type='submit'>Sign Out</button>
     </form>
   )
 }
