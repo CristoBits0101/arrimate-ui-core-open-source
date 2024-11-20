42bb56013f07a5ebbfebd693499774a8b5a3d317
Cristo RPS <cristobits0101@gmail.com>
2024-11-20 13:16:58 +0000

    Updating SVG viewBox

--- src/app/[locale]/(features)/(configuration)/settings/page.tsx
+++ src/app/[locale]/(features)/(configuration)/settings/page.tsx
@@ -1,9 +1,15 @@
 import { auth } from '@/lib/auth'
+import SignOutButton from '@/modules/auth/components/buttons/sign-out/sign-out-button'
 
 const SettingsPage = async () => {
   const session = await auth()
 
-  return <h2>{JSON.stringify(session)}</h2>
+  return (
+    <div>
+      <h2>{JSON.stringify(session)}</h2>
+      <SignOutButton />
+    </div>
+  )
 }
 
 export default SettingsPage
--- src/layouts/components/aside.tsx
+++ src/layouts/components/aside.tsx
@@ -8,7 +8,7 @@ export default function Aside() {
   const component = usePageComponent()
   return (
     <aside
-      className={`hidden md:flex col-span-1 flex-col gap-8 h-screen p-8 sticky top-0 overflow-y-auto ${styles.noScrollbar}`}
+      className={`hidden md:flex col-span-1 flex-col gap-8 min-h-fit h-screen p-8 sticky top-0 overflow-y-auto ${styles.noScrollbar}`}
     >
       <Sidebar />
       {component}
