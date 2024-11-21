7cfab03a6dfb19ae4d65a4fd5a73e636c6737a5e
Cristo RPS <cristobits0101@gmail.com>
2024-11-21 23:18:14 +0000

    Refactoring styles

--- src/modules/auth/components/inputs/email-input.tsx
+++ src/modules/auth/components/inputs/email-input.tsx
@@ -37,8 +37,37 @@ const EmailInput = ({ name, isPending }: EmailInputProps) => {
               className='bg-[#F4F4F4] rounded-none border-[0.094rem] border-solid border-[#EBEAEB] hover:bg-[#EBEAEB] focus:bg-[#EBEAEB] text-[#1d0f0f] placeholder:text-transparent focus:outline-none focus:ring-0 peer'
             />
           </FormControl>
+          {/* 
+            Placeholder: Initial styles 
+            Focus: Focus styles
+            Base: Losing focus
+            */}
           <Label
-            className='absolute left-2 top-1/2 -translate-y-1/2 text-[#1d0f0f] bg-red-100 px-1 text-sm transition-all duration-150 peer-placeholder-shown:top-1/2 peer-placeholder-shown:translate-y-[-50%] peer-placeholder-shown:text-base peer-placeholder-shown:text-[#1d0f0f] peer-focus:top-0 peer-focus:text-sm peer-focus:text-[#1d0f0f]'
+            className='
+              absolute 
+              left-2 
+                    
+              transition-all 
+              duration-150 
+
+              peer-placeholder-shown:px-1 
+              peer-focus:px-1 
+              px-1 
+
+              peer-placeholder-shown:top-0 
+              peer-focus:-top-0 
+              top-0 
+
+              peer-placeholder-shown:translate-y-[-8%] 
+              -translate-y-1/2 
+
+              peer-placeholder-shown:text-base 
+              peer-focus:text-sm 
+              text-xs
+              
+              peer-placeholder-shown:text-[#1d0f0f] 
+              peer-focus:text-[#1d0f0f]
+              text-[#1d0f0f]'
             htmlFor={name}
           >
             {t('inputs.email')}
