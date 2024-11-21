554f320782b31dd74771d9f5eac2a72b178aefad
Cristo RPS <cristobits0101@gmail.com>
2024-11-21 21:33:43 +0000

    Updating input

--- src/modules/auth/components/inputs/email-input.tsx
+++ src/modules/auth/components/inputs/email-input.tsx
@@ -7,6 +7,7 @@ import {
   FormMessage
 } from '@/modules/ui/form'
 import { Input } from '@/modules/ui/input'
+import { Label } from '@/modules/ui/label'
 import { useFormContext } from 'react-hook-form'
 
 // Intl
@@ -26,16 +27,22 @@ const EmailInput = ({ name, isPending }: EmailInputProps) => {
       control={control}
       name={name}
       render={({ field }) => (
-        <FormItem>
+        <FormItem className='relative h-fit'>
           <FormControl>
             <Input
               {...field}
               disabled={isPending}
-              placeholder={t('inputs.email')}
+              placeholder=' '
               type='email'
-              className='bg-[#F4F4F4] rounded-none border-[0.094rem] border-solid border-[#EBEAEB] hover:bg-[#EBEAEB] focus:bg-[#EBEAEB] text-[#1d0f0f] placeholder:text-[#1d0f0f] focus:outline-none focus:ring-0'
+              className='bg-[#F4F4F4] rounded-none border-[0.094rem] border-solid border-[#EBEAEB] hover:bg-[#EBEAEB] focus:bg-[#EBEAEB] text-[#1d0f0f] placeholder:text-transparent focus:outline-none focus:ring-0 peer'
             />
           </FormControl>
+          <Label
+            className='absolute left-2 top-1/2 -translate-y-1/2 text-[#1d0f0f] bg-red-100 px-1 text-sm transition-all duration-150 peer-placeholder-shown:top-1/2 peer-placeholder-shown:translate-y-[-50%] peer-placeholder-shown:text-base peer-placeholder-shown:text-[#1d0f0f] peer-focus:top-0 peer-focus:text-sm peer-focus:text-[#1d0f0f]'
+            htmlFor={name}
+          >
+            {t('inputs.email')}
+          </Label>
           <FormMessage />
         </FormItem>
       )}
