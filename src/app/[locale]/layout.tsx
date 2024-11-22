29667985116f230e4d864c8e94cd065bd78ea8e1
Cristo RPS <cristobits0101@gmail.com>
2024-11-22 21:39:16 +0000

    Updating

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -23,7 +23,6 @@ export default async function LocaleLayout({
   const messages = await getMessages()
   return (
     <html lang={locale}>
-      <head>{/* <link rel='icon' href='/favicon.ico' /> */}</head>
       <body
         className={`${standard.className} grid sm:place-content-center sm:grid-cols-[100%] md:grid-cols-[20rem_1fr_20rem] min-h-screen max-h-fit h-auto min-w-80 w-full bg-[#ffffff] dark:bg-[#1D0F0F] text-[#1d0f0f] dark:text-[#ffffff] text-base`}
       >
