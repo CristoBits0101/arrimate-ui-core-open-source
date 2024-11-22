c97cac4eb22e4e7d3603a199d38215765c3732e9
Cristo RPS <cristobits0101@gmail.com>
2024-11-22 21:49:11 +0000

    Updating settings page

--- src/app/[locale]/(features)/(configuration)/settings/page.tsx
+++ src/app/[locale]/(features)/(configuration)/settings/page.tsx
@@ -1,13 +1,9 @@
-import { auth } from '@/lib/auth'
+// import { auth } from '@/lib/auth'
 
 const SettingsPage = async () => {
-  const session = await auth()
+  // const session = await auth()
 
-  return (
-    <div>
-      <h2>{JSON.stringify(session)}</h2>
-    </div>
-  )
+  return <div>{/* <h2>{JSON.stringify(session)}</h2> */}</div>
 }
 
 export default SettingsPage
