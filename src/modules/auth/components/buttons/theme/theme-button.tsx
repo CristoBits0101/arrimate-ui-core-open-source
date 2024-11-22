2ce5f4dd3cc40da9c773a834bba07eb94b49e835
Cristo RPS <cristobits0101@gmail.com>
2024-11-22 22:29:37 +0000

    Refactoring style

--- src/modules/auth/components/buttons/theme/theme-button.tsx
+++ src/modules/auth/components/buttons/theme/theme-button.tsx
@@ -32,7 +32,7 @@ const ThemeButton: React.FC = () => {
       <Image
         src={iconSrc}
         alt={altText}
-        className='w-5 h-auto object-contain'
+        className='w-5 h-auto object-cover aspect-square'
       />
     </button>
   )
