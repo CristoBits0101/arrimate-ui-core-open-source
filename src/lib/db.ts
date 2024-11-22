f414cf5490390d87ae0eea3c160b1c033cbf36a9
Cristo RPS <cristobits0101@gmail.com>
2024-11-22 17:20:55 +0000

    Updating

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,8 +4,6 @@ declare global {
   var prisma: PrismaClient | undefined
 }
 
-const prisma = global.prisma || new PrismaClient()
+export const db = globalThis.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
-
-export default prisma
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = db
