8bc31af3acf0a229f4c0b800cb03d4b702caee9b
Cristo RPS <cristobits0101@gmail.com>
2024-11-23 19:58:24 +0000

    updating colors

--- src/modules/auth/components/inputs/email-input.tsx
+++ src/modules/auth/components/inputs/email-input.tsx
@@ -33,7 +33,7 @@ const EmailInput = ({ name, isPending }: EmailInputProps) => {
               disabled={isPending}
               placeholder={t('inputs.email')}
               type='email'
-              className='rounded-none border-[0.094rem] border-solid bg-[#F4F4F4] dark:bg-[#303032] border-[#EBEAEB] dark:border-[#424246] hover:bg-[#EBEAEB] focus:bg-[#EBEAEB] dark:hover:bg-[#424246] text-[#1D0F0F] dark:text-[#B9BBDE] placeholder:text-[#1D0F0F] dark:placeholder:text-[#D4DBE2]'
+              className='rounded-none border-[0.094rem] border-solid bg-[#F4F4F4] dark:bg-[#303032] border-[#EBEAEB] dark:border-[#424246] hover:bg-[#EBEAEB] focus:bg-[#EBEAEB] dark:hover:bg-[#424246] dark:focus:bg-[#424246] text-[#1D0F0F] dark:text-[#D4DBE2] placeholder:text-[#453C41] dark:placeholder:text-[#7B7C81]'
             />
           </FormControl>
           <FormMessage />
