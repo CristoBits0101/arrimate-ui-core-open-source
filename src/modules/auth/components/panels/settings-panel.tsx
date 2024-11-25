31a20ad48d1ed92d6fcd5d5a2517132bb9792aa0
Cristo RPS <cristobits0101@gmail.com>
2024-11-25 13:35:10 +0000

    Refactoring call files

--- src/modules/auth/components/panels/settings-panel.tsx
+++ src/modules/auth/components/panels/settings-panel.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import LanguagePanel from '@/modules/auth/components/sections/language-panel'
-import ThemePanel from '@/modules/auth/components/sections/theme-panel'
+import LanguageSection from '@/modules/auth/components/sections/language-section'
+import ThemeSection from '@/modules/auth/components/sections/theme-section'
 
 export default function SettingsPanel() {
   return (
     <div className='flex items-center justify-between absolute w-[calc(100%-40px)] h-fit top-5 left-5'>
-      <ThemePanel />
-      <LanguagePanel />
+      <ThemeSection />
+      <LanguageSection />
     </div>
   )
 }
