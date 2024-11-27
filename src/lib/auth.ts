d0f2e5515dda12eaac6baaf62bfab1f7a03e865a
Cristo RPS <cristobits0101@gmail.com>
2024-11-27 14:20:04 +0000

    Modify auth role

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,17 +1,33 @@
-import authConfig from '@/lib/auth.config'
-import NextAuth from 'next-auth'
+// lib
 import { db } from '@/lib/db'
+
+// modules/auth
+import { getUserById } from '@/modules/auth/data/user-data'
+
+// next-auth
+import authConfig from '@/lib/auth.config'
+import NextAuth, {DefaultSession} from 'next-auth'
+
+// Prisma
 import { PrismaAdapter } from '@auth/prisma-adapter'
+
+// 
+
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   // Configure what user data is returned
   callbacks: {
     async session({ token, session }) {
+      console.log(token)
       if (token.sub && session.user) session.user.id = token.sub
       return session
     },
-    // 
+    //
     async jwt({ token }) {
+      if (!token.sub) return token
+      const existingUser = await getUserById(token.sub)
+      if (!existingUser) return token
+      token.role = existingUser.role
       return token
     }
   },
