06ab4fe98842af6f81d0873fb3799e6be44db3d3
Cristo RPS <cristobits0101@gmail.com>
2024-11-27 15:48:15 +0000

    Updating code

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,20 +6,19 @@ import { getUserById } from '@/modules/auth/data/user-data'
 
 // next-auth
 import authConfig from '@/lib/auth.config'
-import NextAuth, {DefaultSession} from 'next-auth'
+import NextAuth from 'next-auth'
 
 // Prisma
 import { PrismaAdapter } from '@auth/prisma-adapter'
 
-// 
-
-
 export const { handlers, signIn, signOut, auth } = NextAuth({
   // Configure what user data is returned
   callbacks: {
+    //
     async session({ token, session }) {
-      console.log(token)
       if (token.sub && session.user) session.user.id = token.sub
+      if (token.role && session.user)
+        session.user.role = token.role as 'ADMIN' | 'USER'
       return session
     },
     //
@@ -27,7 +26,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       if (!token.sub) return token
       const existingUser = await getUserById(token.sub)
       if (!existingUser) return token
-      token.role = existingUser.role
+      token.role = existingUser.role as 'ADMIN' | 'USER'
       return token
     }
   },
