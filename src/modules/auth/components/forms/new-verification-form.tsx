3572e8a2eab18e94a2954c30394ed38b4ccc7add
Cristo RPS <cristobits0101@gmail.com>
2024-11-30 16:34:45 +0000

    Adding react spinner component

--- src/modules/auth/components/forms/new-verification-form.tsx
+++ src/modules/auth/components/forms/new-verification-form.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import CardWrapper from '@/modules/auth/components/cards/card-wrapper'
+import { BeatLoader } from 'react-spinners'
 
 export default function NewVerificationForm() {
   return (
@@ -9,9 +10,9 @@ export default function NewVerificationForm() {
       redirectButtonLabel='Back to'
       redirectButtonHref='/sign-in'
     >
-      <div className='w-full flex items-center justify-center'>
+      <div className='w-full flex items-center justify-center gap-2'>
         <h2>Confirming your verification</h2>
-
+        <BeatLoader />
       </div>
     </CardWrapper>
   )
