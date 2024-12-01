f3201b00a4c8166f4241ad1dcce1c11c9285194f
Cristo RPS <cristobits0101@gmail.com>
2024-12-01 16:18:30 +0000

    Refactoring

--- src/modules/auth/components/alerts/alert-errors.tsx
+++ src/modules/auth/components/alerts/alert-errors.tsx
@@ -1,13 +1,16 @@
 import { ExclamationTriangleIcon } from '@radix-ui/react-icons'
+import { usePathname } from '@/i18n/routing'
 
 interface FormErrorProps {
   message?: string
 }
 
 export default function FormError({ message }: FormErrorProps) {
+  // Example: /settings
+  const path = usePathname()
   if (!message) return null
   return (
-    <div className='p-3 rounded-none flex items-center gap-x-2 text-red-500'>
+    <div className={`p-3 rounded-none flex items-center gap-x-2 text-red-500 ${path === '/new-verification' ? 'pb-0' : ''}`}>
       <ExclamationTriangleIcon className='h-4 w-4' />
       {message}
     </div>
