c0cbabaf9d6ca238b0b74d694318f3c83bb9c252
Cristo RPS <cristobits0101@gmail.com>
2024-12-01 19:13:28 +0000

    Refactorin messaje

--- src/modules/auth/lib/mail.ts
+++ src/modules/auth/lib/mail.ts
@@ -4,12 +4,21 @@ import { Resend } from 'resend'
 // RESEND_API_KEY
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-export const sendVerificationEmail = async (email: string, token: string, emailMessage: string) => {
+export const sendVerificationEmail = async (
+  email: string,
+  token: string,
+  emailMessage: string
+) => {
   try {
     // Verification link data
     const companyEmail = 'arrimate.team@gmail.com'
     const confirmLink = `http://localhost:3000/new-verification?token=${token}`
-    const messageLink = `<p>Click <a href='${confirmLink}'>here</a> to confirm your registration.</p>`
+
+    // Verification link message
+    const word = 'Confirm';
+    const messageLink = emailMessage.includes(word)
+      ? `<p>Click <a href='${confirmLink}'>here</a> to confirm your registration.</p>`
+      : `<p>Hacer clic <a href='${confirmLink}'>aquí</a> para confirmar el registro.</p>`
 
     // Message sent to user
     await resend.emails.send({
@@ -21,7 +30,9 @@ export const sendVerificationEmail = async (email: string, token: string, emailM
   } catch (error) {
     if (error instanceof Error) {
       console.error(`Error sending email to ${email}:`, error.message)
-      throw new Error('Failed to send verification email. Please try again later.')
+      throw new Error(
+        'Failed to send verification email. Please try again later.'
+      )
     } else {
       console.error(`Unexpected error for ${email}:`, error)
       throw new Error('Unexpected error occurred while sending the email.')
