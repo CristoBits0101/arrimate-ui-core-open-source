bff9e2df20d512e26015b41838023cd7ae7456f2
Cristo RPS <cristobits0101@gmail.com>
2024-12-04 00:25:51 +0000

    Changing colors

--- src/modules/configuration/components/panels/settings-panel.tsx
+++ src/modules/configuration/components/panels/settings-panel.tsx
@@ -16,7 +16,7 @@ export default function SettingsPanel() {
   const { selectedOption, handleSelectOption, handleBack } = useSettings()
 
   return (
-    <div className='w-full h-full rounded-3xl border-[0.05rem] border-[#EBEAEB] border-solid'>
+    <div className='w-full h-full rounded-3xl border-[0.05rem] border-[#EBEAEB] border-solid dark:border-[#3b3b40]'>
       {/* If you click on the options panel the option panel is rendered */}
       {selectedOption === null ? (
         <OptionsPanel
