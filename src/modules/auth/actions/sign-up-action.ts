eb13b5e3667f139d894d515577bf5de11d97d4b3
Cristo RPS <cristobits0101@gmail.com>
2024-12-06 17:59:19 +0000

    Refactoring

--- src/modules/auth/actions/sign-up-action.ts
+++ src/modules/auth/actions/sign-up-action.ts
@@ -1,45 +1,65 @@
 'use server'
 
-// Bcrypt
+// Bcrypt: To encrypt the password
 import bcrypt from 'bcrypt'
 
-// Lib
+// Data: Function that queries the database
 import { generateVerificationToken } from '@/modules/auth/data/token'
 
-// Mail
+// Mail: To send verification email
 import { sendVerificationEmail } from '@/modules/auth/lib/resend'
 
-// Prisma client
+// Prisma: To consult the database
 import { db } from '@/lib/db'
 
-// Zod
+// Zod: To validate data in backend
 import * as z from 'zod'
 import { SignUpSchema } from '@/modules/auth/schemas'
 
-// The data that the user's schema received is saved in value
+// Receive input values ​​and email message
 export default async function signUpAction(
   values: z.infer<typeof SignUpSchema>,
   emailMessage: string
 ) {
-  // Data validation
+  /**
+   * Validation
+   *
+   * 1. Data validation
+   * 2. Returns an error object
+   */
   const validatedFields = SignUpSchema.safeParse(values)
 
-  // Returns an error object
   if (!validatedFields.success) return { error: 'invalid' }
 
-  // Extract fields
+  /**
+   * Validation
+   *
+   * 1. Extract fields
+   * 2. Check password and email match
+   * 3. Regex for special characters
+   */
   const { name, email, password } = validatedFields.data
 
-  // Check password and email match
   if (email.toLocaleLowerCase().trim() === password.toLocaleLowerCase().trim())
     return { error: 'cannot' }
 
-  // Regex for special characters
+  /**
+   * Validation
+   *
+   * 1. Regex for special characters
+   * 2. Check for special character in names
+   */
   const specialCharsRegex = /^[a-zA-Z\u00C0-\u024F\u0400-\u04FF\s]+$/
 
-  // Check for special characters in the name
   if (!specialCharsRegex.test(name)) return { error: 'contain' }
 
+  /**
+   * Validation
+   *
+   * 1. Format email before storing it
+   * 2. We check if the user exists
+   * 3. Returns an error object
+   */
   const cleanedEmail = email.toLocaleLowerCase().trim()
 
   const existingUser = await db.user.findUnique({
@@ -48,14 +68,20 @@ export default async function signUpAction(
     }
   })
 
-  // Returns an error object
   if (existingUser) return { error: 'email' }
 
   /**
-   * Try registration
+   * Registration
+   *
+   * 1. Encrypt the password
+   * 2. Create a new user
+   * 3. Send verification email
+   * 4. Returns an success object
+   * 5. Returns an error object
    */
   try {
     const hashedPassword = await bcrypt.hash(password, 10)
+
     await db.user.create({
       data: {
         name,
@@ -64,18 +90,16 @@ export default async function signUpAction(
       }
     })
 
-    // Send verification email
     const verificationToken = await generateVerificationToken(email)
+
     await sendVerificationEmail(
       verificationToken.email,
       verificationToken.token,
       emailMessage
     )
 
-    // Returns an success object
     return { success: 'confirmation' }
   } catch (error) {
-    // Returns an error object
     return { error: 'registration' }
   }
 }
