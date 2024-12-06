924687129691e8d771fb0569a1dad0ce0eee0bf6
Cristo RPS <cristobits0101@gmail.com>
2024-12-06 15:25:08 +0000

    Adding hooks

--- src/modules/auth/components/forms/sign-up-form.tsx
+++ src/modules/auth/components/forms/sign-up-form.tsx
@@ -1,8 +1,5 @@
 'use client'
 
-// Actions: Encapsulates backend logic
-import signUpAction from '@/modules/auth/actions/sign-up-action'
-
 // Alerts: Serialize backend messages
 import FormError from '@/modules/auth/components/alerts/alert-errors'
 import FormSuccess from '@/modules/auth/components/alerts/alert-success'
@@ -13,9 +10,6 @@ import SubmitButton from '@/modules/auth/components/buttons/submit/submit-form-b
 // Cards: Card to wrap inputs
 import CardWrapper from '@/modules/auth/components/cards/card-wrapper'
 
-// Form: Manage form status and validation
-import { useForm, FormProvider } from 'react-hook-form'
-
 // Inputs: Fillable fields in forms
 import EmailInput from '@/modules/auth/components/inputs/email-input'
 import NameInput from '@/modules/auth/components/inputs/name-input'
@@ -24,72 +18,29 @@ import PasswordInput from '@/modules/auth/components/inputs/password-input'
 // Intl: To get language and set translations
 import { useLocale, useTranslations } from 'next-intl'
 
-// React: Hooks from React
-import { useEffect, useState, useTransition } from 'react'
+// React form: Hooks from React
+import { FormProvider } from 'react-hook-form'
 
 // Shadcn: Contains the form component
 import { Form } from '@/modules/ui/form'
 
-// Zod: Define data validation rules
-import * as z from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { SignUpSchema } from '@/modules/auth/schemas'
+// Custom hook: Encapsulates form logic
+import { useSignUpForm } from '@/modules/auth/hooks/useSignUpForm'
 
 export default function SignUpForm() {
-  const f = useTranslations('Forms')
-  const m = useTranslations('Mail')
-  const subject = m('confirmRegistrationSubject')
-  const b = useTranslations('Button')
+  // Get locale
   const locale = useLocale()
 
-  // Receive messages indicating the outcome of the form submission
-  const [error, setError] = useState<string | undefined>('')
-  const [success, setSuccess] = useState<string | undefined>('')
+  // Get translations
+  const b = useTranslations('Button')
+  const f = useTranslations('Forms')
+  const m = useTranslations('Mail')
+  const s = m('confirmRegistrationSubject')
 
-  // Manage asynchronous operations
-  const [isPending, startTransition] = useTransition()
+  // Managing form logic
+  const { form, error, success, isPending, hydrated, onSubmit } = useSignUpForm(locale, s)
 
-  // Initializes the form and manages its state when rendering
-  const form = useForm<z.infer<typeof SignUpSchema>>({
-    // This validates the form data every time it is submitted
-    resolver: zodResolver(SignUpSchema),
-    mode: 'onSubmit',
-    // Sets the default values for the form fields before any user interaction
-    defaultValues: {
-      name: '',
-      email: '',
-      password: ''
-    }
-  })
-
-  // The form values are initialized with data validated by the schema
-  const onSubmit = (values: z.infer<typeof SignUpSchema>) => {
-    // Reset the error and success messages before attempting submission
-    setError('')
-    setSuccess('')
-    // Send the form data to the server asynchronously
-    startTransition(() => {
-      // Calls the SignUp action with the submitted form values
-      signUpAction(values, subject ?? 'Confirm your registration on Arrímate')
-        .then((data) => {
-          // Set the error message if the server returns one
-          setError(data.error)
-          // Set the success message if the server confirms success
-          setSuccess(data.success)
-        })
-        .catch((err) => {
-          // Handle unexpected errors during submission
-          console.error('Error in SignUp:', err)
-          // Display a generic error message
-          setError('An unexpected error occurred.')
-        })
-    })
-  }
-
-  // Ensure client-side rendering
-  const [hydrated, setHydrated] = useState(false)
-  useEffect(() => setHydrated(true), [])
-
+  // Render the form if the component has hydrated
   return hydrated ? (
     <CardWrapper
       pageNameRedirect={f('signUpForm.pageNameRedirect')}
---
+++ src/modules/auth/hooks/useSignUpForm.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+// Actions: Encapsulates backend logic
+import signUpAction from '@/modules/auth/actions/sign-up-action'
+
+// Form: Manage form status and validation
+import { useForm } from 'react-hook-form'
+
+// React: Hooks from React
+import { useEffect, useState, useTransition } from 'react'
+
+// Zod: Define data validation rules
+import * as z from 'zod'
+import { SignUpSchema } from '@/modules/auth/schemas'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+export function useSignUpForm(locale: string, subject: string) {
+  const [error, setError] = useState<string | undefined>('')
+  const [success, setSuccess] = useState<string | undefined>('')
+  const [hydrated, setHydrated] = useState(false)
+  const [isPending, startTransition] = useTransition()
+
+  // Initialize form with react-hook-form
+  const form = useForm<z.infer<typeof SignUpSchema>>({
+    resolver: zodResolver(SignUpSchema),
+    mode: 'onSubmit',
+    defaultValues: {
+      name: '',
+      email: '',
+      password: ''
+    }
+  })
+
+  // Handle form submission
+  const onSubmit = (values: z.infer<typeof SignUpSchema>) => {
+    setError('')
+    setSuccess('')
+    startTransition(() => {
+      signUpAction(values, subject)
+        .then((data) => {
+          setError(data.error)
+          setSuccess(data.success)
+        })
+        .catch((err) => {
+          console.error('Error in SignUp:', err)
+          setError('An unexpected error occurred.')
+        })
+    })
+  }
+
+  useEffect(() => setHydrated(true), [])
+
+  return {
+    form,
+    error,
+    success,
+    isPending,
+    hydrated,
+    onSubmit
+  }
+}
