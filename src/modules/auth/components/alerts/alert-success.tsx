f937f82a3a4c49f2d49b009c431fa5d76621828e
Cristo RPS <cristobits0101@gmail.com>
2024-12-07 17:21:34 +0000

    Refactoring

--- src/modules/auth/components/alerts/alert-success.tsx
+++ src/modules/auth/components/alerts/alert-success.tsx
@@ -1,13 +1,20 @@
 import { CheckCircledIcon } from '@radix-ui/react-icons'
+import { usePathname } from '@/i18n/routing'
 
 interface FormSuccessProps {
   message?: string
 }
 
 export default function FormSuccess({ message }: FormSuccessProps) {
+  // Example: /settings
+  const path = usePathname()
   if (!message) return null
   return (
-    <div className='p-3 rounded-none flex items-center gap-x-2 text-emerald-500'>
+    <div
+      className={`p-3 rounded-none flex items-center gap-x-2 text-emerald-500 ${
+        path === '/new-verification' || 'unauthorized-access' ? 'pb-0' : ''
+      }`}
+    >
       <CheckCircledIcon className='h-5 w-5' />
       {message}
     </div>
