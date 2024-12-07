73e25e867480fb54a1630a8c2ba3229bce8d2328
Cristo RPS <cristobits0101@gmail.com>
2024-12-07 13:09:33 +0000

    Refactoring

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -2,14 +2,14 @@
 import { Dancing_Script } from 'next/font/google'
 import { Figtree } from 'next/font/google'
 
-// Configure `Dancing_Script` font
+// Configure Dancing_Script font
 export const ds = Dancing_Script({
   display: 'swap',
   subsets: ['latin'],
   weight: ['400', '500', '700']
 })
 
-// Configure `Figtree` font
+// Configure Figtree font
 export const standard = Figtree({
   display: 'swap',
   subsets: ['latin'],
