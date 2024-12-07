0c8452c1859523854d61b6333f0378dbe16eae9f
Cristo RPS <cristobits0101@gmail.com>
2024-12-07 09:45:36 +0000

    r

--- src/app/[locale]/(features)/(configuration)/settings/page.tsx
+++ src/app/[locale]/(features)/(configuration)/settings/page.tsx
@@ -3,7 +3,6 @@ import { auth } from '@/modules/auth/lib/auth'
 // Settings page
 const SettingsPage = async () => {
   const session = await auth()
-
   return <div>{<h2>{JSON.stringify(session)}</h2>}</div>
 }
 
