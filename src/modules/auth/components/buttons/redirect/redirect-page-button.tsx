b27ceeaa96bbfde72b8d4cf620bafdf1971b4dc8
Cristo RPS <cristobits0101@gmail.com>
2024-12-08 23:28:39 +0000

    Refactoring

--- src/modules/auth/components/buttons/redirect/redirect-page-button.tsx
+++ src/modules/auth/components/buttons/redirect/redirect-page-button.tsx
@@ -14,13 +14,13 @@ export default function RedirectButton({
   page
 }: RedirectButtonProps) {
   return (
-    <button
-      className='mx-auto font-normal w-full hover:no-underline text-[#1d0f0f]'
-    >
+    <button className='mx-auto font-normal w-full hover:no-underline text-[#1d0f0f]'>
       <Link className='text-base dark:text-[#ececed]' href={href}>
-        {label}
-        <span className='mx-1'> </span>
-        <span className='font-medium text-blue-600/75 dark:text-[#FFD700]'>{page}</span>
+        {label && label}
+        {label && <span className='ml-1'> </span>}
+        <span className='font-medium text-blue-600/75 dark:text-[#FFD700]'>
+          {page && page}
+        </span>
       </Link>
     </button>
   )
