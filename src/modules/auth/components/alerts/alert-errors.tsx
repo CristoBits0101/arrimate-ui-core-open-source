e19adf7364893b208c52aabd28a9afb0b1359ff8
Cristo RPS <cristobits0101@gmail.com>
2024-12-09 02:47:21 +0000

    Refactoring

--- src/modules/auth/components/alerts/alert-errors.tsx
+++ src/modules/auth/components/alerts/alert-errors.tsx
@@ -1,14 +1,21 @@
+// icons
 import { ExclamationTriangleIcon } from '@radix-ui/react-icons'
+
+// routing
 import { usePathname } from '@/i18n/routing'
 
+// interface
 interface FormErrorProps {
   message?: string
 }
 
+// Show error message
 export default function FormError({ message }: FormErrorProps) {
   // Example: /settings
   const path = usePathname()
+  // Exit if there are no errors
   if (!message) return null
+  // View errors
   return (
     <div className={`p-3 rounded-none flex items-center gap-x-2 text-[#F74201] ${path === '/new-verification' || 'unauthorized-access' ? 'pb-0' : ''}`}>
       <ExclamationTriangleIcon className='h-4 w-4' />
--- src/modules/auth/components/alerts/alert-success.tsx
+++ src/modules/auth/components/alerts/alert-success.tsx
@@ -1,14 +1,21 @@
+// icons
 import { CheckCircledIcon } from '@radix-ui/react-icons'
+
+// routing
 import { usePathname } from '@/i18n/routing'
 
+// interface
 interface FormSuccessProps {
   message?: string
 }
 
+// Show success message
 export default function FormSuccess({ message }: FormSuccessProps) {
   // Example: /settings
   const path = usePathname()
+  // Exit if no message exists
   if (!message) return null
+  // View success
   return (
     <div
       className={`p-3 rounded-none flex items-center gap-x-2 text-emerald-500 ${
