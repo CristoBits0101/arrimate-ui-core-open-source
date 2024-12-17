b722b77d161a09cc5514388f8005f2d11dd4a8c2
Cristo RPS <cristobits0101@gmail.com>
2024-12-17 22:37:53 +0000

    Refactoring formulario

--- src/modules/feeds/components/forms/event-form.tsx
+++ src/modules/feeds/components/forms/event-form.tsx
@@ -1,3 +1,13 @@
+import CardWrapper from '@/modules/feeds/components/cards/card-wrapper'
+import { useTranslations } from 'next-intl'
+
 export default function EventForm() {
-  return <div>Event form</div>
+  const t = useTranslations('feedForms')
+  return (
+    <CardWrapper headerTitle={t('story')}>
+      <div className='w-full h-full flex items-center justify-center bg-[#F4F4F4] rounded-bl-3xl'>
+        EventForm
+      </div>
+    </CardWrapper>
+  )
 }
--- src/modules/feeds/components/forms/story-form.tsx
+++ src/modules/feeds/components/forms/story-form.tsx
@@ -1,9 +1,13 @@
 import CardWrapper from '@/modules/feeds/components/cards/card-wrapper'
+import { useTranslations } from 'next-intl'
 
 export default function StoryForm() {
+  const t = useTranslations('feedForms')
   return (
-    <CardWrapper headerTitle='Hola'>
-      <div className='w-full h-full flex items-center justify-center bg-[#F4F4F4] rounded-bl-3xl'>StoryForm</div>
+    <CardWrapper headerTitle={t('story')}>
+      <div className='w-full h-full flex items-center justify-center bg-[#F4F4F4] rounded-bl-3xl'>
+        StoryForm
+      </div>
     </CardWrapper>
   )
 }
