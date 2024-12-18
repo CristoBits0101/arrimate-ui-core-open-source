c29ef12cb098ed1b892fa1fb1d77eaf56dcfcaaa
Cristo RPS <cristobits0101@gmail.com>
2024-12-18 20:35:10 +0000

    Adding route upload

--- src/modules/feeds/components/forms/story-form.tsx
+++ src/modules/feeds/components/forms/story-form.tsx
@@ -5,15 +5,27 @@ import { useTranslations } from 'next-intl'
 
 export default function StoryForm() {
   const t = useTranslations('feedForms')
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    // handle form submission
+  }
+
   return (
     <CardWrapper headerTitle={t('stories')}>
-      <input
-        type='file'
-        onChange={(e) => {
-          console.log(e.target.files)
-        }}
-        className=''
-      />
+      <form
+        method='post'
+        encType='multipart/form-data'
+        onSubmit={onSubmit}
+      >
+        <input
+          type='file'
+          onChange={(e) => {
+            console.log(e.target.files)
+          }}
+          className=''
+        />
+      </form>
     </CardWrapper>
   )
 }
