44b6da4e48746a695df24393516a3e239302e76f
Cristo RPS <cristobits0101@gmail.com>
2024-12-19 15:06:21 +0000

    Updating logic

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -5,6 +5,7 @@ import { NextResponse } from 'next/server'
 export async function POST(request: Request) {
   // Get the form data
   const formData = await request.formData()
+  // Get the file from the form data
   const file = formData.get('file')
   // Check if a file was uploaded
   if (!file || !(file instanceof Blob))
--- src/modules/feeds/components/forms/story-form.tsx
+++ src/modules/feeds/components/forms/story-form.tsx
@@ -1,18 +1,29 @@
 'use client'
 
 import CardWrapper from '@/modules/feeds/components/cards/card-wrapper'
+import { useState } from 'react'
 import { useTranslations } from 'next-intl'
 
 export default function StoryForm() {
+  // State for uploaded file
+  const [file, setFile] = useState<File | null>(null)
   // Translations
   const t = useTranslations('feedForms')
   // Submit logic
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    // Prevent default form submission
     e.preventDefault()
+    // Create new instance of FormData
+    const formData = new FormData(e.currentTarget)
+    // Add file to form data
+    formData.append('file', file as Blob)
+    // Send form data to server
     const response = await fetch('/api/upload', {
       method: 'POST',
-      body: new FormData(e.currentTarget)
+      headers: { 'content-type': 'multipart/form-data' },
+      body: formData
     })
+    // Get the response data from server
     const data = await response.json()
     console.log(data)
   }
@@ -24,7 +35,10 @@ export default function StoryForm() {
           type='file'
           name='file'
           onChange={(e) => {
-            console.log(e.target.files)
+            // Get the file from the input
+            const file = e.target.files?.[0]
+            // Set the file in state
+            if (file) setFile(file)
           }}
           accept='image/*'
         />
