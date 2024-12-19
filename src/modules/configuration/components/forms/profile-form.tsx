66578df231f7b2e5959dd99f69ea1482bb528133
Cristo RPS <cristobits0101@gmail.com>
2024-12-19 23:54:51 +0000

    Refactoring form

--- src/modules/configuration/components/forms/profile-form.tsx
+++ src/modules/configuration/components/forms/profile-form.tsx
@@ -1,3 +1,31 @@
+'use client'
+
+// Alerts: Serialize backend messages
+import FormError from '@/modules/auth/components/alerts/alert-errors'
+import FormSuccess from '@/modules/auth/components/alerts/alert-success'
+
+// Buttons: Button to send form
+import SubmitButton from '@/modules/auth/components/buttons/submit/submit-form-button'
+
+// Cards: Card to wrap inputs
+import CardWrapper from '@/modules/auth/components/cards/card-wrapper'
+
+// Custom: Encapsulates form logic
+import { useSignInForm } from '@/modules/auth/hooks/useSignInForm'
+
+// Form: Hooks from React
+import { FormProvider } from 'react-hook-form'
+
+// Inputs: Fillable fields in forms
+import EmailInput from '@/modules/auth/components/inputs/email-input'
+import PasswordInput from '@/modules/auth/components/inputs/password-input'
+
+// Intl: To get language and set translations
+import { useLocale, useTranslations } from 'next-intl'
+
+// Shadcn: Contains the form component
+import { Form } from '@/modules/ui/form'
+
 export default function ProfileForm() {
   return <form>profile-form</form>
 }
