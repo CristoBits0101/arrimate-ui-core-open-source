751ec344af884dda3acf434e5fefc4c592e654cc
Cristo RPS <cristobits0101@gmail.com>
2024-12-20 22:41:34 +0000

    Deleting bg

--- src/modules/configuration/components/panels/options-panel.tsx
+++ src/modules/configuration/components/panels/options-panel.tsx
@@ -30,7 +30,7 @@ const OptionsPanel: React.FC<OptionsPanelProps> = ({
   return (
     <div className='flex flex-col items-center'>
       {/* Panel title */}
-      <div className='relative w-full h-fit py-4 border-b-[0.05rem] border-[#EBEAEB] border-solid bg-[#EDECED]'>
+      <div className='relative w-full h-fit py-4 border-b-[0.05rem] border-[#EBEAEB] border-solid'>
         <h2 className='w-full text-center text-lg font-medium'>
           {t('settings')}
         </h2>
