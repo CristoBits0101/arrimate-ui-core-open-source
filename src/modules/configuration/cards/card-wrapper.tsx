a114f0213eb210a61a17227b07a354bbcf98f0f4
Cristo RPS <cristobits0101@gmail.com>
2024-12-20 20:07:19 +0000

    Updating

--- src/modules/configuration/cards/card-wrapper.tsx
+++ src/modules/configuration/cards/card-wrapper.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import UserArticle from '@/modules/configuration/components/articles/user-article'
 import { Card, CardContent, CardFooter, CardHeader } from '@/modules/ui/card'
 
 interface CardWrapperProps {
@@ -9,7 +10,9 @@ interface CardWrapperProps {
 export default function CardWrapper({ children }: CardWrapperProps) {
   return (
     <Card className='relative w-[22rem] shadow-none rounded-none p-5 text-inherit border-[0.094rem] border-solid dark:border-[#3b3b40] border-[#EBEAEB] bg-transparent'>
-      <CardHeader>Card header</CardHeader>
+      <CardHeader>
+        <UserArticle />
+      </CardHeader>
       <CardContent>{children}</CardContent>
       <CardFooter className='px-5 flex flex-col'>Card footer</CardFooter>
     </Card>
--- src/modules/configuration/components/content/profile-settings.tsx
+++ src/modules/configuration/components/content/profile-settings.tsx
@@ -1,10 +1,8 @@
-import UserArticle from '@/modules/configuration/components/articles/user-article'
 import ProfileForm from '@/modules/configuration/components/forms/profile-form'
 
 export default function ProfileSettings() {
   return (
     <div className='w-full h-full flex flex-col items-center'>
-      <UserArticle />
       <ProfileForm />
     </div>
   )
