c4130bd580acbeeadb7ab3f9d6bf3d150c9b0df6
Cristo RPS <cristobits0101@gmail.com>
2024-12-21 19:41:24 +0000

    Actualizando el input del slogan

--- src/modules/configuration/components/inputs/slogan-input.tsx
+++ src/modules/configuration/components/inputs/slogan-input.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useEffect, useState } from 'react'
+import { useUserSession } from '@/modules/configuration/hooks/sessions/useUserSession'
 import {
   FormLabel,
   FormControl,
@@ -19,8 +21,16 @@ interface SloganInputProps {
 
 const SloganInput = ({ name, isPending }: SloganInputProps) => {
   const t = useTranslations('Forms')
+  const { session, hydrated } = useUserSession()
+  const [userSlogan, setUserSlogan] = useState<string | undefined>(undefined)
+
+  useEffect(() => {
+    if (hydrated) setUserSlogan(session?.user?.slogan || '')
+  }, [hydrated, session])
+
   const { control } = useFormContext()
-  return (
+
+  return hydrated ? (
     <FormField
       control={control}
       name={name}
@@ -34,8 +44,8 @@ const SloganInput = ({ name, isPending }: SloganInputProps) => {
               {...field}
               disabled={isPending}
               type='text'
+              placeholder={userSlogan}
               id='slogan'
-              placeholder={t('inputs.slogan')}
               className='rounded-none border-[0.094rem] border-solid bg-[#F4F4F4] dark:bg-[#26272c] border-[#EBEAEB] dark:border-[#3b3b40] hover:bg-[#EBEAEB] focus:bg-[#EBEAEB] dark:hover:bg-[#3b3b40] dark:focus:bg-[#3b3b40] text-[#1D0F0F] dark:text-[#D4DBE2] placeholder:text-[#453C41] dark:placeholder:text-[#848489]'
             />
           </FormControl>
@@ -43,7 +53,7 @@ const SloganInput = ({ name, isPending }: SloganInputProps) => {
         </FormItem>
       )}
     />
-  )
+  ) : null
 }
 
 export default SloganInput
