74fb9da5521dc42c6ce7d25967b3ffdf90cd9aea
Cristo RPS <cristobits0101@gmail.com>
2024-12-24 11:25:50 +0000

    Add user settings fields and country/city tables to database schema

---
+++ prisma/migrations/20241224112523_adding_user_settings/migration.sql
@@ -0,0 +1,46 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "address" TEXT,
+ADD COLUMN     "birthdate" TIMESTAMP(3),
+ADD COLUMN     "cityId" TEXT,
+ADD COLUMN     "countryId" TEXT,
+ADD COLUMN     "interests" TEXT[],
+ADD COLUMN     "occupation" TEXT,
+ADD COLUMN     "portfolioUrl" TEXT,
+ADD COLUMN     "slogan" TEXT,
+ADD COLUMN     "zipCode" TEXT;
+
+-- CreateTable
+CREATE TABLE "Country" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+
+    CONSTRAINT "Country_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "City" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "countryId" TEXT NOT NULL,
+
+    CONSTRAINT "City_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Country_name_key" ON "Country"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Country_code_key" ON "Country"("code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "City_name_countryId_key" ON "City"("name", "countryId");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_countryId_fkey" FOREIGN KEY ("countryId") REFERENCES "Country"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_cityId_fkey" FOREIGN KEY ("cityId") REFERENCES "City"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "City" ADD CONSTRAINT "City_countryId_fkey" FOREIGN KEY ("countryId") REFERENCES "Country"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
