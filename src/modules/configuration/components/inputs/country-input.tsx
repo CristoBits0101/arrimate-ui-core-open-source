b08763d18384c911c2afc696406d5fc54cfedff0
Cristo RPS <cristobits0101@gmail.com>
2024-12-25 17:25:09 +0000

    Refactor CountryInput to simplify props and remove prediction handling for improved clarity

--- src/modules/configuration/components/inputs/country-input.tsx
+++ src/modules/configuration/components/inputs/country-input.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useFormContext } from 'react-hook-form'
-import { useEffect, useState, useRef } from 'react'
 import { useTranslations } from 'next-intl'
 import { useUserSession } from '@/modules/configuration/hooks/sessions/useUserSession'
 import {
@@ -13,64 +12,16 @@ import {
 } from '@/modules/ui/form'
 import { Input } from '@/modules/ui/input'
 
-type Prediction = {
-  description: string
-  place_id: string
-}
-
 interface InputProps {
   name: string
   isPending: boolean
-  setValue(value: string): void
-  predictions?: Prediction[]
+  country: string
 }
 
-const CountryInput = ({
-  name,
-  isPending,
-  predictions,
-  setValue
-}: InputProps) => {
-  const dropdownRef = useRef<HTMLUListElement>(null)
+const CountryInput = ({ name, isPending, country }: InputProps) => {
   const t = useTranslations('Forms')
   const { control } = useFormContext()
-  const { session, hydrated } = useUserSession()
-  const [filteredPredictions, setFilteredPredictions] = useState<Prediction[]>(
-    []
-  )
-
-  const handleOnChange = (value: string, onChange: (value: string) => void) => {
-    setValue(value)
-    onChange(value)
-    const filtered = predictions?.filter((prediction) =>
-      prediction.description.toLowerCase().includes(value.toLowerCase())
-    )
-    if (filtered) setFilteredPredictions(filtered)
-  }
-
-  useEffect(() => {
-    function handleClickOutside(event: MouseEvent) {
-      if (
-        dropdownRef.current &&
-        !dropdownRef.current.contains(event.target as Node)
-      ) {
-        setFilteredPredictions([])
-      }
-    }
-    document.addEventListener('mousedown', handleClickOutside)
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
-    }
-  }, [])
-
-  const handlePredictionClick = (
-    prediction: Prediction,
-    onChange: (value: string) => void
-  ) => {
-    setValue(prediction.description)
-    onChange(prediction.description)
-    setFilteredPredictions([])
-  }
+  const { hydrated } = useUserSession()
 
   return hydrated ? (
     <FormField
@@ -82,36 +33,17 @@ const CountryInput = ({
             {t('inputs.country')}
           </FormLabel>
           <FormControl>
-            <div className='relative'>
-              <Input
-                {...field}
-                disabled={isPending}
-                type='search'
-                id='country'
-                placeholder={session?.user?.country || ''}
-                value={field.value || ''}
-                onChange={(e) => handleOnChange(e.target.value, field.onChange)}
-                className='text-sm rounded-none border-[0.094rem] border-solid bg-[#F4F4F4] dark:bg-[#26272c] border-[#EBEAEB] dark:border-[#3b3b40] hover:bg-[#EBEAEB] focus:bg-[#EBEAEB] dark:hover:bg-[#3b3b40] dark:focus:bg-[#3b3b40] text-[#1D0F0F] dark:text-[#EBEBEC] placeholder:text-[#453C41] dark:placeholder:text-[#848489]'
-              />
-              {filteredPredictions.length > 0 && (
-                <ul
-                  ref={dropdownRef}
-                  className='absolute z-10 bg-white dark:bg-[#26272c] border border-[#EBEAEB] dark:border-[#3b3b40] shadow-md w-full max-h-40 overflow-y-auto border-t-0'
-                >
-                  {filteredPredictions.map((prediction) => (
-                    <li
-                      key={prediction.place_id}
-                      className='text-sm px-3 py-1 cursor-pointer hover:bg-[#EBEAEB] dark:hover:bg-[#3b3b40] rounded-none'
-                      onClick={() =>
-                        handlePredictionClick(prediction, field.onChange)
-                      }
-                    >
-                      {prediction.description}
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </div>
+            <Input
+              {...field}
+              disabled={isPending}
+              type='text'
+              id='country'
+              placeholder={country || ''}
+              readOnly
+              value={field.value || ''}
+              onChange={(e) => field.onChange(e.target.value)}
+              className='text-sm rounded-none border-[0.094rem] border-solid bg-[#F4F4F4] dark:bg-[#26272c] border-[#EBEAEB] dark:border-[#3b3b40] hover:bg-[#EBEAEB] focus:bg-[#EBEAEB] dark:hover:bg-[#3b3b40] dark:focus:bg-[#3b3b40] text-[#1D0F0F] dark:text-[#EBEBEC] placeholder:text-[#453C41] dark:placeholder:text-[#848489]'
+            />
           </FormControl>
           <FormMessage />
         </FormItem>
