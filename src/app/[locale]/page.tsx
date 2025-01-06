789d320545339790e8e490b1d87e0031fc1f1197
Cristo RPS <cristobits0101@gmail.com>
2025-01-06 01:52:08 +0000

    Refactor useThemeSection hook to initialize theme state with fallback for server-side rendering; remove unused getTheme function and apply theme on mount

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,34 +1,20 @@
 'use client'
 
-// Custom hook
-import useThemeSection from '@/modules/configuration/hooks/sections/useThemeSection'
-
-// Layouts
 import Header from '@/layouts/components/header'
 import Aside from '@/layouts/components/aside'
-
-// Posts
 import ShowPostImages from '@/modules/feeds/components/posts/show-post-images'
 import ShowPostStories from '@/modules/feeds/components/posts/show-post-stories'
-
-// Styles
 import '@/styles/globals.css'
 
 export default function StoriesPage() {
-  const { getTheme } = useThemeSection()
-  getTheme()
   return (
     <>
-      {/* Header layout */}
       <Header />
-      {/* Stories page content */}
       <main className='hidden md:flex min-w-[46rem] min-h-screen max-h-fit h-auto col-span-1 flex-col items-center gap-16 dark:bg-[#26272C]'>
         <ShowPostStories />
         <ShowPostImages />
       </main>
-      {/* Aside layout */}
       <Aside />
-      {/* Mobile view message */}
       <div className='sm:grid md:hidden place-content-center w-full h-full'>
         <h1 className='w-fit m-auto'>
           🚧 Mobile version coming soon!
--- src/modules/configuration/hooks/sections/useThemeSection.tsx
+++ src/modules/configuration/hooks/sections/useThemeSection.tsx
@@ -1,16 +1,19 @@
 'use client'
 
-// React hooks
 import { useEffect, useState } from 'react'
 
 // Allowed themes
 type Theme = 'system' | 'dark' | 'light'
 
 const useThemeSection = () => {
-  // Manage the status of selected theme
+  // Initialize theme state from localStorage
   const [theme, setTheme] = useState<Theme>(() => {
-    const storedTheme = localStorage.getItem('theme') as Theme | null
-    return storedTheme || 'system'
+    if (typeof window !== 'undefined') {
+      const storedTheme = localStorage.getItem('theme') as Theme | null
+      return storedTheme || 'system'
+    }
+    // Fallback for server-side rendering
+    return 'system'
   })
 
   /**
@@ -19,22 +22,16 @@ const useThemeSection = () => {
    */
   const changeTheme = (selectedTheme: Theme): void => {
     setTheme(selectedTheme)
+    applyTheme(selectedTheme)
   }
 
-  // Detect the current theme
-  useEffect(() => {
-    applyTheme(theme)
-  }, [theme])
-
   /**
-   * Apply the theme selected
-   * @param selectedTheme
+   * Apply the selected theme
+   * @param theme
    */
   const applyTheme = (theme: Theme): void => {
-    // 1. Get the HTML element
     const root = document.documentElement
 
-    // 2. Apply the selected theme to the root element
     switch (theme) {
       case 'dark':
         root.setAttribute('data-mode', 'dark')
@@ -51,18 +48,14 @@ const useThemeSection = () => {
     }
   }
 
-  /**
-   * Get the stored theme
-   */
-  const getTheme = () => {
-    const storedTheme = localStorage.getItem('theme') as Theme | null
-    changeTheme(storedTheme || 'system')
-  }
+  // Apply theme on mount if it changes
+  useEffect(() => {
+    applyTheme(theme)
+  }, [theme])
 
   return {
     theme,
-    changeTheme,
-    getTheme
+    changeTheme
   }
 }
 
