911df964e500d4b165b59a6b3b2fc460c9224b94
Cristo RPS <cristobits0101@gmail.com>
2025-01-06 22:56:03 +0000

    Refactor stories icons: replace old SVGs with new light and dark variants

--- src/modules/feeds/components/links/stories-link.tsx
+++ src/modules/feeds/components/links/stories-link.tsx
@@ -1,5 +1,11 @@
-import storiesBlackSVG from '@/modules/feeds/assets/icons/links/black/stories.svg'
-import storiesWhiteSVG from '@/modules/feeds/assets/icons/links/white/stories.svg'
+// Dark icons
+import storiesBlackSVG from '@/modules/feeds/assets/icons/links/black/stories-light.svg'
+import storiesWhiteSVG from '@/modules/feeds/assets/icons/links/white/stories-light.svg'
+
+// Light icons
+// import storiesDarkBlackSVG from '@/modules/feeds/assets/icons/links/black/stories-dark.svg'
+// import storiesDarkWhiteSVG from '@/modules/feeds/assets/icons/links/white/stories-dark.svg'
+
 import NavigationItem from '@/modules/navigation/components/links/navbar-link'
 
 export default function stories() {
