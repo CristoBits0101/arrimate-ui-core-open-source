7d3ee04ce5597caed4527b1bf46665063de69326
Cristo RPS <cristobits0101@gmail.com>
2025-01-06 01:54:49 +0000

    Remove hardcoded light mode from LocaleLayout component

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -37,7 +37,7 @@ export default async function LocaleLayout({
   const messages = await getMessages()
   return (
     // HTML
-    <html lang={locale} data-mode='light'>
+    <html lang={locale} data-mode=''>
       {/* Body */}
       <body
         className={`${figtree.className} grid sm:place-content-center sm:grid-cols-[100%] md:grid-cols-[20rem_1fr_20rem] min-h-screen max-h-fit h-auto min-w-80 w-full bg-[#ffffff] dark:bg-[#1B1A1F] text-[#1d0f0f] dark:text-[#ececed] text-base`}
