9822a98dd6e9c0737fb3b62a745e051a5e6086f2
Cristo RPS <cristobits0101@gmail.com>
2025-01-06 01:52:21 +0000

    Remove unused getTheme function call from AuthLayout component

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -1,14 +1,9 @@
-// Custom hook
-import useThemeSection from '@/modules/configuration/hooks/sections/useThemeSection'
-
 // Auth layout
 export default function AuthLayout({
   children
 }: {
   children: React.ReactNode
 }) {
-  const { getTheme } = useThemeSection()
-  getTheme()
   return (
     // General main element for auth pages
     <main className='min-w-52 col-span-3 flex justify-center items-center h-fit'>
