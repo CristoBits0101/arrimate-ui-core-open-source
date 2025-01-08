8cf8034bd6afb7d091961bf94151a2a598245f54
Cristo RPS <cristobits0101@gmail.com>
2025-01-08 19:30:58 +0000

    Refactor comments for clarity in user-data.ts; standardize phrasing for fetching users by email and ID

--- src/data/users/user-data.ts
+++ src/data/users/user-data.ts
@@ -1,19 +1,24 @@
+// Access database client
 import { db } from '@/lib/orm/prisma-client'
 
-// Find user by email
+// Fetch user by email
 export const getUserByEmail = async (email: string) => {
   try {
+    // Search by email
     const user = await db.user.findUnique({ where: { email } })
+    // Return user if found
     return user
   } catch {
     return null
   }
 }
 
-// Find user by id
+// Fetch user by ID
 export const getUserById = async (id: string) => {
   try {
+    // Search by ID
     const user = await db.user.findUnique({ where: { id } })
+    // Return user if found
     return user
   } catch {
     return null
