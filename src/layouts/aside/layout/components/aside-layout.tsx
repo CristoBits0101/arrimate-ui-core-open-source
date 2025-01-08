f48be80b7017d089ce688f31fddc5cb400e1b5c3
Cristo RPS <cristobits0101@gmail.com>
2025-01-08 23:43:41 +0000

    Refactor aside layout styles; rename scrollbar module to aside module for improved organization

--- src/layouts/aside/layout/components/aside-layout.tsx
+++ src/layouts/aside/layout/components/aside-layout.tsx
@@ -7,7 +7,7 @@ import { usePageComponent } from '@/layouts/aside/layout/hooks/useAsidePanel'
 import Sidebar from '@/layouts/aside/sidebar/sidebar-menu'
 
 // Styles
-import styles from '@/layouts/aside/layout/styles/scrollbar.module.css'
+import styles from '@/layouts/aside/layout/styles/aside.module.css'
 
 // Layout
 export default function Aside() {
