0300eab8647c0837861df491f7fbd72062b98713
Cristo RPS <cristobits0101@gmail.com>
2025-01-09 22:33:55 +0000

    Refactor header components; move branding logo, country span, publication date section, and user info section to promotional module and update imports

--- src/layouts/header/layout/header-layout.tsx
+++ src/layouts/header/layout/header-layout.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 // Branding
-import Logo from '@/modules/marketing/company-icons/branding-logo'
+import Logo from '@/layouts/header/logo/branding-logo'
 
 // Navigation
 import ReturnSearchEngine from '@/layouts/header/panels/components/navigation-panel'
