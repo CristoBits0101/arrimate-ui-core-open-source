01e5d57d4212fd4785d2b00eb3ff7665dc8b4020
Cristo RPS <cristobits0101@gmail.com>
2025-01-10 06:41:18 +0000

    Refactor navigation icons; update paths and implement theme-based icon rendering

--- src/modules/navigation/menu/components/sidebar-items/chats-link.tsx
+++ src/modules/navigation/menu/components/sidebar-items/chats-link.tsx
@@ -3,16 +3,23 @@
 // Components
 import NavigationItem from '@/modules/navigation/menu/components/sidebar-link/sidebar-link'
 
+// Context
+import { useThemeContext } from '@/modules/configuration/settings-panel/hooks/useThemeContext'
+
 // Icons
-import chatBlackSVG from '@/assets/icons/navigation/active/light-theme/messages/chats-light-icon.svg'
-import chatWhiteSVG from '@/assets/icons/navigation/inactive/light-theme/messages/chats-light-icon.svg'
+import chatDarkBlackSVG from '@/assets/icons/navigation/active/dark-theme/messages/chats-dark-icon.svg'
+import chatDarkWhiteSVG from '@/assets/icons/navigation/inactive/dark-theme/messages/chats-dark-icon.svg'
+import chatLightBlackSVG from '@/assets/icons/navigation/active/light-theme/messages/chats-light-icon.svg'
+import chatLightWhiteSVG from '@/assets/icons/navigation/inactive/light-theme/messages/chats-light-icon.svg'
 
 export default function Chats() {
+  const { activeTheme } = useThemeContext()
+
   return (
     <NavigationItem
       route='chats'
-      blackIcon={chatBlackSVG}
-      whiteIcon={chatWhiteSVG}
+      blackIcon={activeTheme === 'light' ? chatLightBlackSVG : chatDarkBlackSVG}
+      whiteIcon={activeTheme === 'light' ? chatLightWhiteSVG : chatDarkWhiteSVG}
     />
   )
 }
--- src/modules/navigation/menu/components/sidebar-items/post-link.tsx
+++ src/modules/navigation/menu/components/sidebar-items/post-link.tsx
@@ -3,16 +3,23 @@
 // Components
 import NavigationItem from '@/modules/navigation/menu/components/sidebar-link/sidebar-link'
 
+// Context
+import { useThemeContext } from '@/modules/configuration/settings-panel/hooks/useThemeContext'
+
 // Icons
-import postBlackSVG from '@/assets/icons/navigation/active/light-theme/feeds/post-light-icon.svg'
-import postWhiteSVG from '@/assets/icons/navigation/inactive/light-theme/feeds/post-light-icon.svg'
+import postDarkBlackSVG from '@/assets/icons/navigation/active/dark-theme/feeds/post-dark-icon.svg'
+import postDarkWhiteSVG from '@/assets/icons/navigation/inactive/dark-theme/feeds/post-dark-icon.svg'
+import postLightBlackSVG from '@/assets/icons/navigation/active/light-theme/feeds/post-light-icon.svg'
+import postLightWhiteSVG from '@/assets/icons/navigation/inactive/light-theme/feeds/post-light-icon.svg'
 
 export default function Post() {
+  const { activeTheme } = useThemeContext()
+
   return (
     <NavigationItem
       route='post'
-      blackIcon={postBlackSVG}
-      whiteIcon={postWhiteSVG}
+      blackIcon={activeTheme === 'light' ? postLightBlackSVG : postDarkBlackSVG}
+      whiteIcon={activeTheme === 'light' ? postLightWhiteSVG : postDarkWhiteSVG}
     />
   )
 }
