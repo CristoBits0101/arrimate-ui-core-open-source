6c41361efe12d88a2c46b3e567e3cdbdef9e8e51
Cristo RPS <cristobits0101@gmail.com>
2025-01-16 17:49:54 +0000

    Updating styles

--- src/modules/configuration/settings-panel/buttons/back-button.tsx
+++ src/modules/configuration/settings-panel/buttons/back-button.tsx
@@ -1,8 +1,14 @@
 'use client'
 
+// Context
+import { useThemeContext } from '@/modules/configuration/settings-panel/hooks/useThemeContext'
+
+// Icons
+import backDarkIcon from '@/assets/icons/buttons/inactive/dark-theme/windows/back-dark-icon.svg'
+import backLightIcon from '@/assets/icons/buttons/inactive/light-theme/windows/back-light-icon.svg'
+
 // Image
 import Image from 'next/image'
-import backIcon from '@/assets/icons/buttons/inactive/light-theme/windows/back-light-icon.svg'
 
 // Type props
 interface BackButtonProps {
@@ -10,6 +16,8 @@ interface BackButtonProps {
 }
 
 const BackButton: React.FC<BackButtonProps> = ({ onClick }) => {
+  // Context
+  const { activeTheme } = useThemeContext()
   return (
     <button
       className='w-6 absolute left-8 top-1/2 transform -translate-y-1/2 flex items-center bg-[#F4F4F4] dark:bg-[#26272C] aspect-square rounded-full border-[0.05rem] dark:border-[#3b3b40] border-[#EBEAEB] border-solid'
@@ -18,7 +26,7 @@ const BackButton: React.FC<BackButtonProps> = ({ onClick }) => {
       <Image
         width={24}
         height={24}
-        src={backIcon}
+        src={activeTheme === 'light' ? backLightIcon : backDarkIcon}
         alt='Back button icon'
         className='aspect-square object-contain'
       />
