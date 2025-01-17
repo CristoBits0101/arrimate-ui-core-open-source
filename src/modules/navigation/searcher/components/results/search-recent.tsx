27b7d3355357a07a90f3ec0cbb584033d2fffacb
Cristo RPS <cristobits0101@gmail.com>
2025-01-17 23:55:16 +0000

    Refactoring svg

--- src/modules/navigation/searcher/components/results/search-recent.tsx
+++ src/modules/navigation/searcher/components/results/search-recent.tsx
@@ -3,17 +3,28 @@
 // Components
 import ResultsBox from '@/modules/navigation/searcher/components/boxes/results-box'
 
+// Context
+import { useThemeContext } from '@/modules/configuration/settings-panel/hooks/useThemeContext'
+
 // Icons
-import historyIcon from '@/assets/icons/buttons/inactive/light-theme/searcher/history-light-icon.svg'
+import historyDarkIcon from '@/assets/icons/buttons/inactive/dark-theme/searcher/history-dark-icon.svg'
+import historyLightIcon from '@/assets/icons/buttons/inactive/light-theme/searcher/history-light-icon.svg'
 
 // Intl
 import { useTranslations } from 'next-intl'
 
 export default function SearchRecent() {
+  // Context
+  const { activeTheme } = useThemeContext()
+
   // Translations
   const t = useTranslations('Searcher')
 
   return (
-    <ResultsBox iconSrc={historyIcon} h2={t('recent')} closeButton={true} />
+    <ResultsBox
+      iconSrc={activeTheme === 'light' ? historyLightIcon : historyDarkIcon}
+      h2={t('recent')}
+      closeButton={true}
+    />
   )
 }
--- src/modules/navigation/searcher/components/results/search-recommended.tsx
+++ src/modules/navigation/searcher/components/results/search-recommended.tsx
@@ -3,19 +3,26 @@
 // Components
 import ResultsBox from '@/modules/navigation/searcher/components/boxes/results-box'
 
+// Context
+import { useThemeContext } from '@/modules/configuration/settings-panel/hooks/useThemeContext'
+
 // Icons
-import searchIcon from '@/assets/icons/buttons/inactive/light-theme/searcher/search-light-icon.svg'
+import searchDarkIcon from '@/assets/icons/buttons/inactive/dark-theme/searcher/search-dark-icon.svg'
+import searchLightIcon from '@/assets/icons/buttons/inactive/light-theme/searcher/search-light-icon.svg'
 
 // Intl
 import { useTranslations } from 'next-intl'
 
 export default function SearchRecommended() {
+  // Context
+  const { activeTheme } = useThemeContext()
+
   // Translations
   const t = useTranslations('Searcher')
 
   return (
     <ResultsBox
-      iconSrc={searchIcon}
+      iconSrc={activeTheme === 'light' ? searchLightIcon : searchDarkIcon}
       h2={t('recommended')}
       clearButton
       closeButton
