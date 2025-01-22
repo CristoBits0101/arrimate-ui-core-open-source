fc64bcea7c5d639a9de841402fddbce227f3821a
Cristo Suárez <cristobits0101@gmail.com>
2025-01-22 17:20:28 +0000

    Updating middleware and deleting auth.js

--- src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,5 +0,0 @@
-// Import authentication request handlers
-import { handlers } from '@/lib/auth/auth'
-
-// Export GET and POST methods for NextAuth.js
-export const { GET, POST } = handlers
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,50 +1,8 @@
-// Auth
-import authConfig from '@/lib/auth/auth.config'
-import NextAuth from 'next-auth'
-
-// Intl
 import createMiddleware from 'next-intl/middleware'
-import { routing } from '@/i18n/routing'
-
-// Routes
-import { API_AUTH_ROUTE, AUTH_ROUTES, PUBLIC_ROUTES } from '@/config/routes'
-
-// Configuration
-const { auth } = NextAuth(authConfig)
-const intlMiddleware = createMiddleware(routing)
-
-export default auth((request) => { 
-  // Get origin
-  const origin = request.nextUrl.origin
-
-  // Get pathname
-  const pathname = request.nextUrl.pathname
-
-  // Check login
-  const isLoggedIn = !!request.auth
-
-  // Check routes
-  const isApiAuthRoute = pathname.startsWith(API_AUTH_ROUTE)
-  const isAuthRoute = AUTH_ROUTES.includes(pathname)
-  const isPublicRoute = PUBLIC_ROUTES.includes(pathname)
-
-  // Check API
-  if (isApiAuthRoute) return
-
-  // Redirect stories
-  if (isLoggedIn && isAuthRoute) request.nextUrl.href = `${origin}/`
-
-  // Redirect sign-in
-  if (!isLoggedIn && !isPublicRoute) request.nextUrl.href = `${origin}/sign-in`
-
-  // Apply language
-  const response = intlMiddleware(request)
-
-  // Return path
-  return response
-})
-
-// Run middleware
+import {routing} from './i18n/routing'
+ 
+export default createMiddleware(routing)
+ 
 export const config = {
   matcher: [
     '/',
